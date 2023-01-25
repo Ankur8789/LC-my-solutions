c1700a8051aea0659221bbc45761894e9ceb16dc
Ankur8789 <109090385+Ankur8789@users.noreply.github.com>
2023-01-25 14:52:17 +0530

    Time: 596 ms (21.61%), Space: 200.2 MB (8.87%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
@@ -1,83 +1,58 @@
+void bfs(unordered_map<int,vector<int>> &adj,vector<int> &dist,int src)
+{
+    queue<int> q;
+    q.push(src);
+    dist[src]=0;
+    while(q.size())
+    {
+        int curr=q.front();
+        q.pop();
+        for(auto child:adj[curr])
+        {
+            if(dist[child]>dist[curr]+1)
+            {
+                dist[child]=dist[curr]+1;
+                q.push(child);
+            }
+        }
+    }
+}
+
 class Solution {
 public:
-     vector<int> adj[100001];
-    int vis1[100001];
-    int vis2[100001];
-    int dis1[100001];
-    int dis2[100001];
-    int closestMeetingNode(vector<int>& edges, int node1, int node2)
-    {     //building graph
-          for(int i=0;i<edges.size();i++)
-          {
-              if(edges[i]==-1)
-                  continue;
-              adj[i].push_back(edges[i]);
-          }
-        //1st bfs
-         queue<int> q1;
-        dis1[node1]=0;
-        vis1[node1]=1;
-        q1.push(node1);
-        int k=1;
-        while(q1.size()>0)
+    int closestMeetingNode(vector<int>& edges, int node1, int node2){
+        unordered_map<int,vector<int>> adj;
+        int n=edges.size();
+        for(int i=0;i<n;i++)
         {
-            int sz=q1.size();
-            while(sz--)
-            {
-                int x=q1.front();
-                q1.pop();
-                for(auto t: adj[x])
-                {
-                    if(vis1[t]==0)
-                    {
-                        dis1[t]=k;
-                        q1.push(t);
-                        vis1[t]=1;
-                    }
-                }
-            }
-            k++;
+            if(edges[i]==-1)
+                continue;
+            adj[i].push_back(edges[i]);
         }
-        //2nd bfs
-        queue<int> q2;
-        dis2[node2]=0;
-        vis2[node2]=1;
-        q2.push(node2);
-        k=1;
-        while(q2.size()>0)
+        vector<int> dist1(n,1e9);
+        vector<int> dist2(n,1e9);
+        bfs(adj,dist1,node1);
+        bfs(adj,dist2,node2);
+        for(int i=0;i<n;i++)
         {
-            int sz=q2.size();
-            while(sz--)
-            {
-                int x=q2.front();
-                q2.pop();
-                for(auto t: adj[x])
-                {
-                    if(vis2[t]==0)
-                    {
-                        dis2[t]=k;
-                        q2.push(t);
-                        vis2[t]=1;
-                    }
-                }
-            }
-            k++;
+            // cout<<dist1[i]<<" ";
         }
-            int mx=INT_MAX,idx=-1;
-        for(int i=0;i<edges.size();i++)
-        {  //cout<<dis1[i]<<" "<<dis2[i]<<endl;
-            if(vis1[i]==1 && vis2[i]==1)
+        cout<<endl;
+        int ans=-1,val=1e9;
+        for(int i=0;i<n;i++)
+        {
+            // cout<<dist2[i]<<" ";
+            if(dist1[i]!=1e9&&dist2[i]!=1e9)
             {
-                if(max(dis1[i],dis2[i])<mx)
+                int value=max(dist1[i],dist2[i]);
+                if(val>value)
                 {
-                    mx=max(dis1[i],dis2[i]);
-                    idx=i;
+                    val=value;
+                    ans=i;
                 }
             }
         }
-        return idx;
-        
-        
-        
+        // cout<<endl;
+        return ans;
     }
 };
