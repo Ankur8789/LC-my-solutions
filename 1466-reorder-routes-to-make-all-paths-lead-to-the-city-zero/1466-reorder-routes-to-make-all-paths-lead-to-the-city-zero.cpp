6dd1b975e887f89b163b8f6434207af145836f37
Ankur8789 <109090385+Ankur8789@users.noreply.github.com>
2023-01-27 10:35:18 +0530

    Time: 563 ms (54.81%), Space: 114.3 MB (59.35%) - LeetHub

---
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    typedef long long ll;
+    vector<pair<ll,ll>> adj[100001];
+    ll vis[100001];
+    int minReorder(int n, vector<vector<int>>& cnc)
+    {
+        for(auto t: cnc)
+        {
+            adj[t[0]].push_back({t[1],0});
+            adj[t[1]].push_back({t[0],1});
+        }
+        ll cc=0;
+        vis[0]=1;
+        queue<ll> q;
+        q.push(0);
+        while(q.size()>0)
+        {
+            ll x=q.front();
+            q.pop();
+            for(auto t: adj[x])
+            {  //cout<<x<<"->"<<t.first<<endl;
+                if(vis[t.first]==1)
+                    continue;
+                if(t.second==0)
+                    cc++;
+                q.push(t.first);
+                vis[t.first]=1;
+            }
+        }
+        return cc;
+        
+    }
+};
