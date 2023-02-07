f223f74047ebaa732bf321f41907c44aa19603d3
Ankur8789 <109090385+Ankur8789@users.noreply.github.com>
2023-02-08 00:40:37 +0530

    Time: 48 ms (100.00%), Space: 26.8 MB (67.23%) - LeetHub

--- 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.cpp
+++ 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.cpp
@@ -1,8 +1,12 @@
+#define fastio()                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
 class Solution {
 public:
     typedef long long ll;
     int longestBeautifulSubstring(string s) 
-    {
+    {   fastio();
         ll ax=0,ex=0,ix=0,ox=0,ux=0;
         ll i=0,j=0,n=s.size();
         ll res=0;
