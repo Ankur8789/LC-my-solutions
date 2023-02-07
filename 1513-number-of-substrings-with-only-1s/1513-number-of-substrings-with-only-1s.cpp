eb1565d4a46d420fba38d82588995c7c3e489839
Ankur8789 <109090385+Ankur8789@users.noreply.github.com>
2023-02-08 00:50:18 +0530

    Time: 9 ms (84.16%), Space: 8.8 MB (83.73%) - LeetHub

--- 1513-number-of-substrings-with-only-1s/1513-number-of-substrings-with-only-1s.cpp
+++ 1513-number-of-substrings-with-only-1s/1513-number-of-substrings-with-only-1s.cpp
@@ -4,6 +4,9 @@ public:
     const ll mod=1e9+7;
     int numSub(string s) 
     {
+        ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
         ll cnt=0,zr=0,i=0,j=0,n=s.length();
         while(j<n)
         {
