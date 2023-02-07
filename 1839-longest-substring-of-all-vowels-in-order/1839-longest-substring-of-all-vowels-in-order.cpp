2f3fc04ef5a64f989cc69bdda8339d0a08222963
Ankur8789 <109090385+Ankur8789@users.noreply.github.com>
2023-02-08 00:39:49 +0530

    Time: 105 ms (57.77%), Space: 26.8 MB (67.23%) - LeetHub

---
+++ 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.cpp
@@ -0,0 +1,101 @@
+class Solution {
+public:
+    typedef long long ll;
+    int longestBeautifulSubstring(string s) 
+    {
+        ll ax=0,ex=0,ix=0,ox=0,ux=0;
+        ll i=0,j=0,n=s.size();
+        ll res=0;
+        while(j<n)
+        {
+            if(s[j]=='a')
+                ax++;
+            if(s[j]=='e')
+                ex++;
+            if(s[j]=='i')
+                ix++;
+            if(s[j]=='o')
+                ox++;
+            if(s[j]=='u')
+                ux++;
+            //sliding
+            if(s[j]=='a')
+            {
+                while(ex>0 || ix>0 || ox>0 || ux>0)
+                {
+                    if(s[i]=='a')
+                        ax--;
+                    if(s[i]=='e')
+                        ex--;
+                    if(s[i]=='i')
+                        ix--;
+                    if(s[i]=='o')
+                        ox--;
+                    if(s[i]=='u')
+                        ux--;
+                    i++;
+                }
+            }
+            else if(s[j]=='e')
+            {
+                while(ix>0 || ox>0 || ux>0)
+                {
+                    if(s[i]=='a')
+                        ax--;
+                    if(s[i]=='e')
+                        ex--;
+                    if(s[i]=='i')
+                        ix--;
+                    if(s[i]=='o')
+                        ox--;
+                    if(s[i]=='u')
+                        ux--;
+                    i++;
+                }
+            
+            }
+            else if(s[j]=='i')
+            {
+                while(ox>0 || ux>0)
+                {
+                    if(s[i]=='a')
+                        ax--;
+                    if(s[i]=='e')
+                        ex--;
+                    if(s[i]=='i')
+                        ix--;
+                    if(s[i]=='o')
+                        ox--;
+                    if(s[i]=='u')
+                        ux--;
+                    i++;
+                }
+            
+            }
+            else if(s[j]=='o')
+            {
+                while(ux>0)
+                {
+                    if(s[i]=='a')
+                        ax--;
+                    if(s[i]=='e')
+                        ex--;
+                    if(s[i]=='i')
+                        ix--;
+                    if(s[i]=='o')
+                        ox--;
+                    if(s[i]=='u')
+                        ux--;
+                    i++;
+                }
+            
+            }
+            if(ax>0 && ex>0 && ix>0 && ox>0 && ux>0)
+                res=max(res,j-i+1);
+            j++;
+            
+        }
+        return res;
+        
+    }
+};
