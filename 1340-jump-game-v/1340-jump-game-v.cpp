af56fdda2a52738da903399aed824ef46cd08017
Ankur8789 <109090385+Ankur8789@users.noreply.github.com>
2023-02-09 02:59:29 +0530

    Time: 44 ms (68.99%), Space: 14.6 MB (81.12%) - LeetHub

--- 1340-jump-game-v/1340-jump-game-v.cpp
+++ 1340-jump-game-v/1340-jump-game-v.cpp
@@ -1,22 +1,27 @@
+#define fastio()                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
 class Solution {
 public:
     
    int dp[1001];
-    int solve(vector<int> &arr,int i,int d,int n)
+    int f(vector<int> &arr,int i,int d,int n)
     {
-        if(dp[i] != -1) return dp[i];
+        if(dp[i] != -1) 
+            return dp[i];
         int ans = 0;
         for(int j=i+1;j<=min(n-1,i+d);j++)
         {
             if(arr[i]>arr[j])
-                ans=max(ans,solve(arr,j,d,n));
+                ans=max(ans,f(arr,j,d,n));
             else
                 break;
         }
         for(int j=i-1;j>=max(i-d,0);j--)
         {
             if(arr[i] > arr[j])
-               ans=max(ans,solve(arr,j,d,n));
+               ans=max(ans,f(arr,j,d,n));
             else
                 break;
         }
@@ -24,13 +29,14 @@ public:
         
     }
     int maxJumps(vector<int>& arr, int d)
-    {
+    {   
+        fastio();
         memset(dp,-1,sizeof(dp));
         int ans = 0;
         int n = arr.size();
         for(int i=0;i<n;i++)
         {
-            ans = max(ans,solve(arr,i,d,n));
+            ans = max(ans,f(arr,i,d,n));
         }
         return ans;
     }
