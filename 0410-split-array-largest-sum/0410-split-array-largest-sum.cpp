cf70615a6ffd57c93fefecb487cc46319480bce8
Ankur8789 <109090385+Ankur8789@users.noreply.github.com>
2023-02-15 15:49:33 +0530

    Time: 388 ms (14.68%), Space: 7.9 MB (15.06%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -3,43 +3,45 @@
     cin.tie(NULL);                    \
     cout.tie(NULL);
 typedef long long ll;
-class Solution {
+class Solution
+{
 public:
     ll dp[1001][51];
-    ll f(ll i,vector<ll>& pre,ll k,vector<int>& nums)
+    ll f(ll i, vector<ll> &pre, ll k, vector<int> &nums)
     {
-       if(k==0)
-       {  if(i!=0)
-          return pre[pre.size()-1]-pre[i-1];
-        else
-            return pre[pre.size()-1];
-       }
-       if(i==pre.size())
-           return 1e9;
-       if(dp[i][k]!=-1)
-           return dp[i][k];
-       ll sum=0;
-       ll ans=1e9;
-       for(ll idx=i;idx<pre.size();idx++)
-       {
-          sum+=nums[idx];
-          ans=min(ans,max(sum,f(idx+1,pre,k-1,nums)));
-       }
-        return dp[i][k]=ans;
-    }
-    int splitArray(vector<int>& nums, int k) 
-    {     
-       fastio();
-        ll n=nums.size();
-        vector<ll> pre(n);
-        ll sm=0;
-        for(ll i=0;i<n;i++)
+        if (k == 0)
         {
-            sm+=nums[i];
-            pre[i]=sm;
+            if (i != 0)
+                return pre[pre.size() - 1] - pre[i - 1];
+            else
+                return pre[pre.size() - 1];
         }
-        memset(dp,-1,sizeof(dp));
-        ll ans=f(0,pre,k-1,nums);
+        if (i == pre.size())
+            return 1e9;
+        if (dp[i][k] != -1)
+            return dp[i][k];
+        ll sum = 0;
+        ll ans = 1e9;
+        for (ll idx = i; idx < pre.size(); idx++)
+        {
+            sum += nums[idx];
+            ans = min(ans, max(sum, f(idx + 1, pre, k - 1, nums)));
+        }
+        return dp[i][k] = ans;
+    }
+    int splitArray(vector<int> &nums, int k)
+    {
+        fastio();
+        ll n = nums.size();
+        vector<ll> pre(n);
+        ll sm = 0;
+        for (ll i = 0; i < n; i++)
+        {
+            sm += nums[i];
+            pre[i] = sm;
+        }
+        memset(dp, -1, sizeof(dp));
+        ll ans = f(0, pre, k - 1, nums);
         return ans;
     }
 };
