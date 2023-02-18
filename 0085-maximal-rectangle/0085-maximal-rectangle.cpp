4f0c5fa75a32849a281f5e62a61c33236de88c77
Ankur8789 <109090385+Ankur8789@users.noreply.github.com>
2023-02-18 12:46:15 +0530

    Time: 196 ms (8.01%), Space: 13.3 MB (72.77%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -0,0 +1,60 @@
+//largest are histogram solution ...but i dont think it is dp
+class Solution {
+public:
+    int mah(vector<int>& heights) {
+        int res = 0, curr = 0, n = heights.size();
+        stack<int> s;
+        for(int i=0;i<n;i++)
+        {
+            while(s.empty() == false && heights[s.top()]>=heights[i]){
+                int top = s.top();
+                s.pop();
+                if(s.empty())
+                    curr = heights[top]*i;
+                else
+                    curr = heights[top]*(i-s.top()-1);
+                res = max(curr,res);
+            }
+            s.push(i);
+        }
+       
+        while(s.empty() == false)
+        {
+                int top = s.top();
+                s.pop();
+                if(s.empty())
+                    curr = heights[top]*n;
+                else
+                    curr = heights[top]*(n-s.top()-1);
+                res = max(curr,res);
+            }
+        return res;
+    }
+    
+    
+    
+    
+    int maximalRectangle(vector<vector<char>>& matrix) 
+    {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        int ans=-1e5;
+        vector<int> hs(m);
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(matrix[i][j]=='0')
+                    hs[j]=0;
+                else
+                    hs[j]+=1;
+            }
+            for(auto t: hs)cout<<t<<" ";
+            cout<<endl;
+            ans=max(ans,mah(hs));
+            
+            
+        }
+        return ans;
+    }
+};
