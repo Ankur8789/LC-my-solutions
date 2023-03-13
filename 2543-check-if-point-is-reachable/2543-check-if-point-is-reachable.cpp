cc75f53a0e2186f879a6eb052de1d9d7d9e7dff0
Ankur8789 <109090385+Ankur8789@users.noreply.github.com>
2023-03-13 13:44:20 +0530

    Time: 3 ms (39.39%), Space: 5.9 MB (80.36%) - LeetHub

--- 2543-check-if-point-is-reachable/2543-check-if-point-is-reachable.cpp
+++ 2543-check-if-point-is-reachable/2543-check-if-point-is-reachable.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
+    // this kind of problems require thinking from the back
     bool isReachable(int tx, int ty)
     {
-       if(tx==1 && ty==1)
-           return true;
-        int xgcd=__gcd(tx,ty);
+        if(tx==1 || ty==1)
+            return true;
+        int p=__gcd(tx,ty);
         int cc=0;
         for(int i=0;i<32;i++)
         {
-            if((xgcd>>i)&1)
+            if((p>>i)&1)
                 cc++;
         }
-        if(cc==1)
-            return true;
-        else
-            return false;
+        return cc==1;
     }
 };
