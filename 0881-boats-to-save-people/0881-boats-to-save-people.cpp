687372b6c715a848ff19001b9c64a84b11a21f39
Ankur8789 <109090385+Ankur8789@users.noreply.github.com>
2023-04-03 14:09:40 +0530

    Time: 99 ms (16.94%), Space: 42 MB (59.84%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.cpp
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
-    bool check(int mid,vector<int>& pe, int lt)
+    int numRescueBoats(vector<int>& pe, int lt) 
     {
+        sort(pe.begin(),pe.end());
         int i=0,j=pe.size()-1;
         int cc=0;
         while(i<=j)
@@ -24,24 +25,6 @@ public:
             }
             
         }
-        return cc<=mid;
-    }
-    int numRescueBoats(vector<int>& pe, int lt) 
-    {
-        int l=0,r=1e9;
-        sort(pe.begin(),pe.end());
-        int ans=-1;
-        while(l<=r)
-        {
-            int mid=(l+r)/2;
-            if(check(mid,pe,lt))
-            {
-               ans=mid;
-                r=mid-1;
-            }
-            else
-                l=mid+1;
-        }
-        return ans;
+        return cc;
     }
 };
