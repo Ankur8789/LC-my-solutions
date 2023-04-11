02b8ac4e47e3aaf307354315e8c945bc55f3c4a4
Ankur8789 <109090385+Ankur8789@users.noreply.github.com>
2023-04-11 14:23:29 +0530

    Time: 245 ms (26.33%), Space: 60.9 MB (37.72%) - LeetHub

---
+++ 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.cpp
@@ -0,0 +1,56 @@
+typedef long long ll;
+ll parent[200001];
+ll siz[200001];
+void make(ll v)
+{
+    parent[v] = v;
+    siz[v] = 1;
+}
+ll find(ll v)
+{
+    if (parent[v] == v)
+        return v;
+    else // path compresssion
+        return parent[v] = find(parent[v]);
+}
+void Union(ll a, ll b)
+{
+    a = find(a);
+    b = find(b);
+    if (a != b)
+    { // union by size
+        if (siz[a] < siz[b])
+            swap(a, b);
+        parent[b] = a;
+        siz[a] += siz[b];
+    }
+}
+unordered_map<ll,vector<ll>> mp;
+class Solution {
+public:
+    string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) 
+    {
+        ll n=s.length();
+        for(ll i=0;i<n;i++)
+            make(i);
+        for(auto t: pairs)
+        {
+            Union(t[0],t[1]);
+        }
+        unordered_map<ll,multiset<char>> ms;
+        for(ll i=0;i<n;i++)
+        {
+            ll a=find(i);
+            ms[a].insert(s[i]);
+        }
+        string ans;
+        ans.resize(n);
+        for(ll i=0;i<n;i++)
+        {
+           ll a=find(i);
+            ans[i]=*ms[a].begin();
+            ms[a].erase(ms[a].begin());
+        }
+        return ans;
+    }
+};
