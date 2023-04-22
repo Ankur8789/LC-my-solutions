45ef1a1e55a33e7104ca1747fc3853fe36c331e9
Ankur8789 <109090385+Ankur8789@users.noreply.github.com>
2023-04-22 15:31:24 +0530

    Time: 6 ms (38.80%), Space: 6.6 MB (58.49%) - LeetHub

--- 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.cpp
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.cpp
@@ -1,29 +1,50 @@
+typedef long long ll;
+ll parent[200001];
+ll siz[200001];
+void make(ll v)
+{
+	parent[v]=v;
+	siz[v]=1;
+}
+ll find(ll v)
+{
+	if(parent[v]==v)
+	return v;
+	else  //path compresssion
+	return parent[v]=find(parent[v]);
+}
+void Union(ll a,ll b)
+{
+	a=find(a);
+	b=find(b);
+	if(a!=b)
+	{   //union by size
+		if(siz[a]<siz[b])
+		swap(a,b);
+		parent[b]=a;
+		siz[a]+=siz[b];
+	}
+}
 class Solution {
 public:
-    void unionn(int x, int y, vector<int> &alpha){
-        x = find(x, alpha);
-        y = find(y, alpha);
-
-        if(x!=y)
-            alpha[max(x, y)] = min(x, y);
-    }
-
-    int find(int x, vector<int> &alpha){
-        if(alpha[x]==-1)
-            return x;
-
-        return alpha[x] = find(alpha[x], alpha);
-    }
-
-    string smallestEquivalentString(string s1, string s2, string baseStr) {
-        vector<int> alpha(26, -1);
-
-        for(int i=0; i<s1.size(); i++)
-            unionn(s1[i]-'a', s2[i]-'a', alpha);
-
-        for(int i=0; i<baseStr.size(); i++)
-            baseStr[i] = find(baseStr[i]-'a', alpha)+'a';
-
-        return baseStr;
+    string smallestEquivalentString(string s1, string s2, string bs) 
+    {
+        for(ll i=0;i<26;i++)make(i);
+        for(ll i=0;i<s1.length();i++)
+        Union(s1[i]-'a',s2[i]-'a');
+        string ans="";
+        for(ll i=0;i<bs.size();i++)
+        {
+            ll x=find(bs[i]-'a');
+            for(ll i=0;i<26;i++)
+            {
+                if(parent[i]==x)
+                {
+                    ans+=char(i+'a');
+                    break;
+                }
+            }
+        }
+        return ans;
     }
 };
