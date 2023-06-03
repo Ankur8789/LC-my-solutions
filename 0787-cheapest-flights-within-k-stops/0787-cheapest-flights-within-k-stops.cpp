243e51f881c4bb4bbc848b6c344b061746bc5518
Ankur8789 <109090385+Ankur8789@users.noreply.github.com>
2023-06-03 15:40:02 +0530

    Time: 25 ms (67.06%), Space: 13.7 MB (33.48%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,16 +1,36 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) 
-    {
-       
-        int MAX_VALUE = 100000; vector<int> prices(n, MAX_VALUE); prices[src] = 0;
-        
-        for (int i = 0; i < k+1; i++) 
-        {
-            vector<int> tmpPrices = prices;  
-            for (const auto& flight: flights) tmpPrices[flight[1]] = min(tmpPrices[flight[1]], flight[2] + prices[flight[0]]); 
-            prices = tmpPrices;
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K) {
+        vector<pair<int,int>> adj[n];
+        for(auto it : flights){
+            adj[it[0]].push_back({it[1],it[2]});
         }
-        return prices[dst] == MAX_VALUE? -1: prices[dst];
+
+        vector<int> dist(n,1e9);
+        dist[src] =0;
+        queue<pair<int,pair<int,int>>> q;
+        q.push({0,{src,0}});
+
+        while(!q.empty()){
+            int stop = q.front().first;
+            int node = q.front().second.first;
+            int cost = q.front().second.second;
+            q.pop();
+            
+            if(stop > K) continue;
+
+            for(auto it : adj[node]){
+                int adjNode = it.first;
+                int weight = it.second;
+
+                if(cost + weight < dist[adjNode] && stop <= K){
+                    dist[adjNode] = cost + weight;
+                    q.push({stop+1,{adjNode,dist[adjNode]}});
+                }
+            }
+        }
+
+        if(dist[dst]  == 1e9) return -1;
+        return dist[dst];
     }
 };
