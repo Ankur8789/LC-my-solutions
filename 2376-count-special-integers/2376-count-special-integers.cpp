3cf15d51b2953b2770d4e67a1c8199f496dd48d1
Ankur8789 <109090385+Ankur8789@users.noreply.github.com>
2023-06-25 16:38:34 +0530

    Time: 13 ms (45.66%), Space: 6.3 MB (18.50%) - LeetHub

--- 2376-count-special-integers/2376-count-special-integers.cpp
+++ 2376-count-special-integers/2376-count-special-integers.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    int dp[11][2][2000][2];
     int f(int i,int tight,string&s,int msk,int taken)
     {
         if(i==s.size())
@@ -9,6 +10,8 @@ public:
             else
                 return 0;
         }
+        if(dp[i][tight][msk][taken]!=-1)
+            return dp[i][tight][msk][taken];
         int ans=0;
         int limit=9;
         if(tight)
@@ -35,11 +38,11 @@ public:
                     ans+=f(i+1,(tight & (idx==limit)),s,msk|(1<<idx),taken);
             }
         }
-        return ans;
+        return dp[i][tight][msk][taken]=ans;
     }
     int countSpecialNumbers(int n) 
     {
-       
+       memset(dp,-1,sizeof(dp));
        string s=to_string(n);
        int ans=f(0,1,s,0,0);
        return ans;
