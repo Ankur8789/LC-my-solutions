7d60bbbda074b554a34d5e59ed63df5ac9b7b81d
Ankur8789 <109090385+Ankur8789@users.noreply.github.com>
2023-07-09 18:30:00 +0530

    Time: 1206 ms (5.03%), Space: 93.9 MB (5.03%) - LeetHub

---
+++ 2272-substring-with-largest-variance/2272-substring-with-largest-variance.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    int largestVariance(string s)
+    {
+        int ans=0;
+        unordered_map<char,int> mp;
+        for(auto t: s)
+            mp[t]++;
+        for(char ch='a';ch<='z';ch++)
+        {
+            for(char p='a';p<='z';p++)
+            {
+                if(ch==p)
+                    continue;
+                if(mp.find(ch)==mp.end() || mp.find(p)==mp.end())
+                    continue;
+                int res=0;
+                int cnt=0;
+                map<char,int> cc;
+                for(auto t: s)
+                {
+                    if(t==ch)
+                        cnt++,cc[t]++;
+                    if(t==p)
+                        cnt--,cc[t]++;
+                    
+                    if(cc.size()==2)
+                    res=max(res,cnt);
+                    if(cnt<0)
+                    {
+                        cnt=0;cc.clear();
+                    }
+                }
+                cc.clear();
+                reverse(s.begin(),s.end());
+                cnt=0;
+                for(auto t: s)
+                {
+                    if(t==ch)
+                        cnt++,cc[t]++;
+                    if(t==p)
+                        cnt--,cc[t]++;
+                    
+                    if(cc.size()==2)
+                    res=max(res,cnt);
+                    if(cnt<0)
+                    {
+                        cnt=0;cc.clear();
+                    }
+                }
+                // cout<<ch<<" "<<p<<" "<<res<<" "<<cnt<<endl;
+                ans=max(ans,res);
+                
+            }
+        }
+        return ans;
+    }
+};
