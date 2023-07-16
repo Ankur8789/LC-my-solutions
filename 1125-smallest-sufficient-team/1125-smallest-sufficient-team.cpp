c3f5ba4da352ac680ecb1314163a0dbb4146c2ad
Ankur8789 <109090385+Ankur8789@users.noreply.github.com>
2023-07-16 16:34:24 +0530

    Time: 2456 ms (5.16%), Space: 135.4 MB (11.29%) - LeetHub

--- 1125-smallest-sufficient-team/1125-smallest-sufficient-team.cpp
+++ 1125-smallest-sufficient-team/1125-smallest-sufficient-team.cpp
@@ -1,56 +1,73 @@
-#define ll long long
-class Solution {
+typedef long long ll;
+ll ppc(ll n)
+{
+    ll c = 0;
+    for (; n; ++c)
+        n &= n - 1;
+    return c;
+}
+ll k;
+class Solution
+{
 public:
-    unordered_map<string, ll> dp;
-    vector<int> mask;
- ll f(int k, int idx, int currmask) {
-        if (currmask == k)
-            return 0;
-
-        if(idx == mask.size())
-            return INT_MAX;
-
-        string key = to_string(idx)+" "+to_string(currmask);
-        if (dp.count(key))
-            return dp[key];
-        ll ans = INT_MAX;
-        ll pick = f(k,idx+1, currmask | mask[idx]);
-        ll notPick = f(k, idx + 1, currmask);
-        if (__builtin_popcountll(pick) < __builtin_popcountll(notPick)) 
-         ans=pick | (1LL << idx);
-         else 
-         ans = notPick;
-
-        dp[key] = ans;
-        return ans;
-    }
-    vector<int> smallestSufficientTeam(vector<string>& req_skills, vector<vector<string>>& people) {
-        unordered_map<string, int> mp;
-        for (int i = 0; i < req_skills.size(); i++) {
-            mp[req_skills[i]] = i;
+    vector<ll> mask;
+    unordered_map<string, ll> mp;
+    map<string, ll> dp;
+    ll f(int i, unordered_set<string> &st, vector<vector<string>> &people,ll msk)
+    {
+        if(i==mask.size())
+        {
+            if(ppc(msk)==st.size())
+                return 0;
+            else
+                return INT_MAX;
         }
+        string val=to_string(i)+" "+to_string(msk);
+        if(dp.count(val))
+            return dp[val];
+        ll a=f(i+1,st,people,msk);
+        ll b=f(i+1,st,people,msk|mask[i]);
+        ll ans=0;
+        if(ppc(a)>ppc(b))
+            ans=b|(1LL<<i);
+        else
+            ans=a;
+        return dp[val]=ans;
+    }
+    vector<int> smallestSufficientTeam(vector<string> &req, vector<vector<string>> &people)
+    {
+        unordered_set<string> st;
+        
+        for (int i = 0; i < req.size(); i++)
+            mp[req[i]] = i;
+        for (auto t : req)
+            st.insert(t);
+        vector<ll> temp;
         mask.resize(people.size(), 0);
-        for (int i = 0; i < people.size(); i++) {
-            vector<string>& temp = people[i];
+        for (int i = 0; i < people.size(); i++)
+        {
+            vector<string> &temp = people[i];
             int t = 0;
-            for (string& skill : temp) {
+            for (string &skill : temp)
+            {
                 t |= 1 << mp[skill];
             }
             mask[i] = t;
         }
-
-        int k = (1 << req_skills.size()) - 1;
+        k = (1 << req.size()) - 1;
         dp.clear();
-        ll cnt = f(k,0,0);
+        ll val = f(0, st, people, 0);
+        ll index=0;
         vector<int> ans;
-        int index = 0;
-        while (cnt > 0) {
-            ll pick = cnt & 1;
-            if (pick == 1) {
+         while (val > 0) 
+         {
+            ll pick = val & 1;
+            if (pick == 1) 
+            {
                 ans.push_back(index);
             }
             index++;
-            cnt >>= 1;
+            val >>= 1;
         }
         return ans;
     }
