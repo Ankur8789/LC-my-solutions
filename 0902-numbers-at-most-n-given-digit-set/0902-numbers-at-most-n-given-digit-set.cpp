9ba87e310ec33193e2ac9a5930d0be8faade8a1e
Ankur8789 <109090385+Ankur8789@users.noreply.github.com>
2023-07-31 17:24:39 +0530

    Time: 5 ms (29.86%), Space: 7.9 MB (68.37%) - LeetHub

--- 0902-numbers-at-most-n-given-digit-set/0902-numbers-at-most-n-given-digit-set.cpp
+++ 0902-numbers-at-most-n-given-digit-set/0902-numbers-at-most-n-given-digit-set.cpp
@@ -1,54 +1,57 @@
 class Solution {
 public:
-    int dp[11][2][2];
-    int f(int i,int tight,unordered_set<string>& st,string s,int taken)
+    int dp[10][2][2];
+    int f(int i,int tight,int taken,string& s,unordered_set<string>& st)
     {
         if(i==s.size())
-            return 1;
+        {
+            if(taken)
+                return 1;
+            else
+                return 0;
+        }
         if(dp[i][tight][taken]!=-1)
             return dp[i][tight][taken];
-        int ans=0;
-        int limit=9;
+        int ub=9;
         if(tight)
-            limit=s[i]-'0';
-        for(int idx=0;idx<=limit;idx++)
+            ub=s[i]-'0';
+        int ans=0;
+        for(int idx=0;idx<=ub;idx++)
         {
-           if(idx==0)
+           if(taken==0)
            {
-              if(taken==0)
-              {
-                  ans+=f(i+1,(tight & (idx==limit)),st,s,taken);
-              }
+               if(idx==0)
+                   ans+=f(i+1,tight&(idx==ub),taken,s,st);
+               else
+               {
+                   if(st.find(to_string(idx))!=st.end())
+                   {
+                       ans+=f(i+1,tight&(idx==ub),taken^1,s,st);
+                   }
+               }
+                   
            }
            else
            {
                if(st.find(to_string(idx))!=st.end())
                {
-                   if(taken)
-                   {
-                       ans+=f(i+1,(tight & (idx==limit)),st,s,taken);
-                   }
-                   else
-                   {
-                       ans+=f(i+1,(tight & (idx==limit)),st,s,taken^1);
-                   }
+                   ans+=f(i+1,tight&(idx==ub),taken,s,st);
                }
            }
         }
         return dp[i][tight][taken]=ans;
-        
+            
     }
     int atMostNGivenDigitSet(vector<string>& digits, int n) 
     {
-        // reverse(digits.begin(),digits.end());
-        // // digits.push_back("0");
-        // reverse(digits.begin(),digits.end());
         unordered_set<string> st;
         for(auto t: digits)
+        {
             st.insert(t);
+        }
         string s=to_string(n);
         memset(dp,-1,sizeof(dp));
-        int ans=f(0,1,st,s,0)-1;
+        int ans=f(0,1,0,s,st);
         return ans;
     }
 };
