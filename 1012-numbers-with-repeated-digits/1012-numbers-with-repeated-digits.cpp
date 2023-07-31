b93677d44af7551ce604999c922ef1bd47b49307
Ankur8789 <109090385+Ankur8789@users.noreply.github.com>
2023-07-31 13:00:52 +0530

    Time: 19 ms (51.97%), Space: 6.6 MB (23.58%) - LeetHub

--- 1012-numbers-with-repeated-digits/1012-numbers-with-repeated-digits.cpp
+++ 1012-numbers-with-repeated-digits/1012-numbers-with-repeated-digits.cpp
@@ -1,53 +1,45 @@
 class Solution {
 public:
-    int dp[11][2][2][1<<11][2];
-    int f(int i,int tight,int taken,string& s,int msk,int dub)
+    int dp[10][2][1<<11][2][2];
+    int f(int i,int tight,int msk,int taken,string& s,int rep)
     {
         if(i==s.size())
         {
-            if(taken && dub)
+            if(taken)
                 return 1;
             else
                 return 0;
         }
-        if(dp[i][tight][taken][msk][dub]!=-1)
-            return dp[i][tight][taken][msk][dub];
-        int limit=9;
+        if(dp[i][tight][msk][taken][rep]!=-1)
+            return dp[i][tight][msk][taken][rep];
+        int ub=9;
         if(tight)
-            limit=s[i]-'0';
+            ub=s[i]-'0';
         int ans=0;
-        for(int idx=0;idx<=limit;idx++)
+        for(int idx=0;idx<=ub;idx++)
         {
-            if(!taken)
+            if(rep==0)
             {
                 if(idx==0)
-                {
-                   ans+=f(i+1,(tight & (idx==limit)),taken,s,msk,dub); 
-                }
+                    ans+=f(i+1,tight&(idx==ub),msk,taken,s,rep);
                 else
-                {
-                    ans+=f(i+1,(tight & (idx==limit)),taken^1,s,msk|(1<<idx),dub);
-                }
+                    ans+=f(i+1,tight&(idx==ub),msk|(1<<idx),taken,s, rep^1);
             }
             else
             {
-               if((msk>>idx)&1)
-               {
-                  ans+=f(i+1,(tight & (idx==limit)),taken,s,msk,1);
-               }
-               else
-               {
-                   ans+=f(i+1,(tight & (idx==limit)),taken,s,msk|(1<<idx),dub);
-               }
+                if((msk>>idx)&1)
+                    ans+=f(i+1,tight&(idx==ub),msk,1,s,rep);
+                else
+                    ans+=f(i+1,tight&(idx==ub),msk|(1<<idx),taken,s,rep);
             }
         }
-        return dp[i][tight][taken][msk][dub]=ans;
+        return dp[i][tight][msk][taken][rep]=ans;
     }
     int numDupDigitsAtMostN(int n) 
     {
-       memset(dp,-1,sizeof(dp));
-       string s=to_string(n);
-       int ans=f(0,1,0,s,0,0);
-       return ans;
+        memset(dp,-1,sizeof(dp));
+        string s=to_string(n);
+        int ans=f(0,1,0,0,s,0);
+        return ans;
     }
 };
