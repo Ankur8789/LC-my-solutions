e453919714c87ab5fa85665ccff4ce850f697f8b
Ankur8789 <109090385+Ankur8789@users.noreply.github.com>
2023-08-04 10:09:47 +0530

    Time: 3 ms (91.38%), Space: 8.4 MB (73.04%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,32 +1,28 @@
-#define fastio() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); 
 class Solution {
 public:
     int dp[301];
     bool f(int i,string& s,unordered_set<string>& st)
     {
-        if(i==s.length())
-            return true; 
+        if(i==s.size())
+            return true;
         if(dp[i]!=-1)
             return dp[i];
-        string temp="";
+        string str="";
         bool ans=false;
-        for(int idx=i;idx<s.length();idx++)
+        for(int idx=i;idx<s.size();idx++)
         {
-            temp+=s[idx];
-            if(st.find(temp)==st.end())
-                continue;
-            bool x=f(idx+1,s,st);
-           if(x)
-               return dp[i]=true;
-           
+            str+=s[idx];
+            if(st.find(str)!=st.end())
+                ans|=f(idx+1,s,st);
+            if(ans)
+                return dp[i]=true;
         }
         return dp[i]=false;
     }
-    bool wordBreak(string s, vector<string>& wordDict)
+    bool wordBreak(string s, vector<string>& wordDict) 
     {
-        fastio();
         unordered_set<string> st;
-        for(auto t:wordDict)
+        for(auto t: wordDict)
             st.insert(t);
         memset(dp,-1,sizeof(dp));
         bool ans=f(0,s,st);
