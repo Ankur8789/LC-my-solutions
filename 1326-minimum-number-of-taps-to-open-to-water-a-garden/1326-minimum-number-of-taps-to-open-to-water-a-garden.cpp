2616b391d94568da76a87dc26c5e4ccf12678d5b
Ankur8789 <109090385+Ankur8789@users.noreply.github.com>
2023-08-12 12:34:48 +0530

    Time: 17 ms (82.14%), Space: 16 MB (26.43%) - LeetHub

---
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    // static bool cmp(pair<int,int>& a,pair<int,int>& b)
+    // {
+    //     if(a.first==b.first)
+    //         return a.second>b.second;
+    //     else
+    //         return a.first<b.first;
+    // }
+    int minTaps(int n, vector<int>& ranges) 
+    {
+        vector<pair<int,int>> vp;
+        for(int i=0;i<ranges.size();i++)
+        {
+            int mn=max(i-ranges[i],0);
+            int mx=min(i+ranges[i],n);
+            vp.push_back({mn,mx});
+            
+        }
+        sort(vp.begin(),vp.end());
+        // for(auto t: vp)
+        //     cout<<t.first<<" "<<t.second<<endl;
+        vector<pair<int,int>> req;
+        req.push_back(vp[0]);
+        int cnt=0;
+        int curr=0;
+        int i=0;
+        while(i<=n)
+        {
+            int last=curr;
+            while(i<=n&&  curr>=vp[i].first)
+            {
+                last=max(last,vp[i].second);
+                i++;
+            }
+            cnt++;
+            if(last==n)
+                return cnt;
+            if(last==curr)
+                return -1;
+            curr=last;
+        }
+      
+        return cnt;
+    }
+};
