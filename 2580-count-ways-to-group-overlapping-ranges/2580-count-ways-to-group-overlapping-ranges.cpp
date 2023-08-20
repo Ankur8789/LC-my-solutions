8adea7e4ced63d16d3048a4baa699867ad92b4a0
Ankur8789 <109090385+Ankur8789@users.noreply.github.com>
2023-08-21 00:31:22 +0530

    Time: 255 ms (88.32%), Space: 72.8 MB (52.45%) - LeetHub

---
+++ 2580-count-ways-to-group-overlapping-ranges/2580-count-ways-to-group-overlapping-ranges.cpp
@@ -0,0 +1,47 @@
+typedef long long ll;
+const ll mod=1e9+7;
+ll F[1000001];
+void ix()
+{
+    F[0] = F[1] = 1;
+    for (ll i = 2; i <= 1000000; i++)
+        F[i] = (F[i - 1] * 1LL * i) % mod;
+}
+ll mpw(ll base, ll exp, ll M)
+{
+    // O(LOGEXP) TIME
+    if (exp == 0)
+        return 1;
+    ll res = mpw(base, exp / 2, M);
+    if (exp % 2 == 1)
+        return (((res * res) % M) * base) % M;
+    else
+        return (res * res) % M;
+}
+class Solution {
+public:
+    int countWays(vector<vector<int>>& ranges) 
+    {
+        // ix();
+        ll cnt=1;
+        sort(ranges.begin(),ranges.end());
+        vector<ll> tot;
+        ll curr=ranges[0][1];
+        for(ll i=1;i<ranges.size();i++)
+        {
+            if(curr>=ranges[i][0])
+            {
+                curr=max(curr,1LL*ranges[i][1]);
+            }
+            else
+            {
+                // cout<<curr<<" "<<ranges[i][0]<<endl;
+                cnt++;
+                curr=ranges[i][1];
+            }
+        }
+        ll ans=mpw(2,cnt,mod);
+        return int(ans);
+        
+    }
+};
