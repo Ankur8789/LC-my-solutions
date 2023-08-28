9f3734a27e6b0326f09c44d5af2db68e93e444b8
Ankur8789 <109090385+Ankur8789@users.noreply.github.com>
2023-08-29 02:13:30 +0530

    Time: 518 ms (10.00%), Space: 242.6 MB (5.00%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
@@ -1,58 +1,68 @@
-void bfs(unordered_map<int,vector<int>> &adj,vector<int> &dist,int src)
-{
-    queue<int> q;
-    q.push(src);
-    dist[src]=0;
-    while(q.size())
-    {
-        int curr=q.front();
-        q.pop();
-        for(auto child:adj[curr])
-        {
-            if(dist[child]>dist[curr]+1)
-            {
-                dist[child]=dist[curr]+1;
-                q.push(child);
-            }
-        }
-    }
-}
-
 class Solution {
 public:
-    int closestMeetingNode(vector<int>& edges, int node1, int node2){
-        unordered_map<int,vector<int>> adj;
-        int n=edges.size();
-        for(int i=0;i<n;i++)
+    int closestMeetingNode(vector<int>& edges, int node1, int node2) 
+    {
+       unordered_map<int,vector<int>> adj;
+       for(int i=0;i<edges.size();i++)
+       {
+           if(edges[i]!=-1)
+               adj[i].push_back(edges[i]);
+       }
+       // node1 se bfs
+       int n=edges.size();
+        vector<int> vis(n),dis1(n,1e9),dis2(n,1e9);
+        multiset<pair<int,int>> ms;
+        dis1[node1]=0;
+        ms.insert({0,node1});
+        while(ms.size()>0)
         {
-            if(edges[i]==-1)
-                continue;
-            adj[i].push_back(edges[i]);
-        }
-        vector<int> dist1(n,1e9);
-        vector<int> dist2(n,1e9);
-        bfs(adj,dist1,node1);
-        bfs(adj,dist2,node2);
-        for(int i=0;i<n;i++)
-        {
-            // cout<<dist1[i]<<" ";
-        }
-        cout<<endl;
-        int ans=-1,val=1e9;
-        for(int i=0;i<n;i++)
-        {
-            // cout<<dist2[i]<<" ";
-            if(dist1[i]!=1e9&&dist2[i]!=1e9)
+            auto t=*ms.begin();
+            ms.erase(ms.begin());
+            int d=t.first;
+            int u=t.second;
+            for(auto t: adj[u])
             {
-                int value=max(dist1[i],dist2[i]);
-                if(val>value)
+                if(dis1[t]>d+1)
                 {
-                    val=value;
-                    ans=i;
+                    if(ms.count({dis1[t],t}))
+                        ms.erase({dis1[t],t});
+                    dis1[t]=d+1;
+                    ms.insert({dis1[t],t});
                 }
             }
         }
-        // cout<<endl;
+        // node 2
+        ms.insert({0,node2});
+        dis2[node2]=0;
+        while(ms.size()>0)
+        {
+            auto t=*ms.begin();
+            ms.erase(ms.begin());
+            int d=t.first;
+            int u=t.second;
+            for(auto t: adj[u])
+            {
+                if(dis2[t]>d+1)
+                {
+                    if(ms.count({dis2[t],t}))
+                        ms.erase({dis2[t],t});
+                    dis2[t]=d+1;
+                    ms.insert({dis2[t],t});
+                }
+            }
+        }
+        int ans=-1,pp=1e9;
+        for(int i=0;i<n;i++)
+        {
+            if(dis1[i]==1e9 || dis2[i]==1e9)
+                continue;
+            int tem=max(dis1[i],dis2[i]);
+            if(tem<pp)
+            {
+                pp=tem;
+                ans=i;
+            }
+        }
         return ans;
     }
 };
