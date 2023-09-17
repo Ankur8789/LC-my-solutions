f0b09318f40d8b38e2a090d8436d33800f92c2f1
Ankur8789 <109090385+Ankur8789@users.noreply.github.com>
2023-09-17 09:34:38 +0530

    Time: 187 ms (18.18%), Space: 127.9 MB (9.09%) - LeetHub

--- 2861-maximum-number-of-alloys/2861-maximum-number-of-alloys.cpp
+++ 2861-maximum-number-of-alloys/2861-maximum-number-of-alloys.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     typedef long long ll;
-    bool check(ll mid,int k,int budget,vector<int>& t, vector<int>& stock, vector<int>& cost)
+    bool check(ll mid,int k,int budget,vector<vector<int>>& comp, vector<int>& stock, vector<int>& cost)
     {
-        
+        for(auto t: comp)
+        {
             vector<ll> req(t.size());
             for(int i=0;i<t.size();i++)
             {
@@ -11,33 +12,33 @@ public:
                 // cout<<req[i]<<" ";
                 req[i]=max(req[i]-stock[i],0*1LL);
             }
+            // cout<<endl;
+            // for(auto t: req)
+            //     cout<<t<<" ";
+            // cout<<endl;
             ll sum=0;
             for(int i=0;i<t.size();i++)
                 sum+=1LL*req[i]*cost[i];
             if(sum<=budget)
                 return 1;
-        
+        }
         return 0;
     }
     int maxNumberOfAlloys(int n, int k, int budget, vector<vector<int>>& comp, vector<int>& stock, vector<int>& cost) 
     {
         sort(comp.begin(),comp.end());
+        ll l=0,r=1e10;
         ll ans=0;
-        for(ll i=0;i<k;i++)
+        while(l<=r)
         {
-            ll l=0,r=1e12;
-        
-            while(l<=r)
+            ll mid=(l+r)/2;
+            if(check(mid,k,budget,comp,stock,cost))
             {
-                ll mid=(l+r)/2;
-                if(check(mid,k,budget,comp[i],stock,cost))
-                {
-                    ans=max(ans,mid);
-                    l=mid+1;
-                }
-                else
-                    r=mid-1;
+                ans=mid;
+                l=mid+1;
             }
+            else
+                r=mid-1;
         }
         // cout<<check(2,k,budget,comp,stock,cost)<<endl;
         return ans;
