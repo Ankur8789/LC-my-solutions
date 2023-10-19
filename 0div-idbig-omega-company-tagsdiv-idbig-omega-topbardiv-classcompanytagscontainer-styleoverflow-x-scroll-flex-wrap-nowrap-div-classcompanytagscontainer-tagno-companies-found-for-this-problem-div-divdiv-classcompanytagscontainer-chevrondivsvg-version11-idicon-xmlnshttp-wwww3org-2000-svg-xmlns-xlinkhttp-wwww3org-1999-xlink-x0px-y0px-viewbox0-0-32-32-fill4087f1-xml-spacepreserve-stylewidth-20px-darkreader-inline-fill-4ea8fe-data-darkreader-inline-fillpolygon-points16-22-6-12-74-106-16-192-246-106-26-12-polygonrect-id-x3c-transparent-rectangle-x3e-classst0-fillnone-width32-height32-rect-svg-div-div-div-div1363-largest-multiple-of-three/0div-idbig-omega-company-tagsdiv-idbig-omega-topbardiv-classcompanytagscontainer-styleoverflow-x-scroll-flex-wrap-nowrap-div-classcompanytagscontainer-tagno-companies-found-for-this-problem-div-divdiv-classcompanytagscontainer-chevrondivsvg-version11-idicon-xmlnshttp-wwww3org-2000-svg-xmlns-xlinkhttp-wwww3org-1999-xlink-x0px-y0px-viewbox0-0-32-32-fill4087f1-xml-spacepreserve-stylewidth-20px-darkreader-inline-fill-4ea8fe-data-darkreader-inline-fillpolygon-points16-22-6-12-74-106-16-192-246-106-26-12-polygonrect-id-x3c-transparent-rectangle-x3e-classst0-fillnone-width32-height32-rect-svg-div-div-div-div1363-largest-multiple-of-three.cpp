9a837d47992df45f8b1b0894a6ed51599f5ebc13
Ankur8789 <109090385+Ankur8789@users.noreply.github.com>
2023-10-20 00:42:44 +0530

    Time: 20 ms (50.22%), Space: 20.1 MB (48.05%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4ea8fe-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1363-largest-multiple-of-three/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4ea8fe-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1363-largest-multiple-of-three.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4ea8fe-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1363-largest-multiple-of-three/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4ea8fe-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1363-largest-multiple-of-three.cpp
@@ -1,145 +1,145 @@
-class Solution {
+
+class Solution
+{
 public:
-    string largestMultipleOfThree(vector<int>& digits) 
+    string largestMultipleOfThree(vector<int> &digits)
     {
-        string res="";
-        sort(digits.begin(),digits.end());
-        reverse(digits.begin(),digits.end());
-        int sum=0;
-        for(auto t: digits)
-            sum+=t;
-        map<int,int> mp;
-            for(auto t: digits)
-                mp[t%3]++;
-        if(sum%3==0)
+        string res = "";
+        sort(digits.begin(), digits.end());
+        reverse(digits.begin(), digits.end());
+        int sum = 0;
+        for (auto t : digits)
+            sum += t;
+        map<int, int> mp;
+        for (auto t : digits)
+            mp[t % 3]++;
+        if (sum % 3 == 0)
         {
-            string res="";
+            string res = "";
             // int zr=0;
-            for(auto t: digits)
-               { res+=char(t+'0');}
-            int zr=0;
-            for(auto t: res)
-                zr+=(t=='0');
-            res=(zr==res.size())?"0":res;
-            return res;
-            
-        }
-        else if(sum%3==1)
-        {
-            
-            if(mp[1])
+            for (auto t : digits)
             {
-               string res="";
-               int idx=digits.size()-1;
-               for(;idx>=0;idx--)
-               {
-                   if(digits[idx]%3==1)
-                       break;
-                   res+=char('0'+digits[idx]); 
-               }
-               for(int i=0;i<idx;i++)
-                   res+=char('0'+digits[i]);
-                sort(res.begin(),res.end());
-                reverse(res.begin(),res.end());
-                 int zr=0;
-            for(auto t: res)
-                zr+=(t=='0');
-            res=(zr==res.size() && zr!=0)?"0":res;
+                res += char(t + '0');
+            }
+            int zr = 0;
+            for (auto t : res)
+                zr += (t == '0');
+            res = (zr == res.size()) ? "0" : res;
             return res;
+        }
+        else if (sum % 3 == 1)
+        {
+
+            if (mp[1])
+            {
+                string res = "";
+                int idx = digits.size() - 1;
+                for (; idx >= 0; idx--)
+                {
+                    if (digits[idx] % 3 == 1)
+                        break;
+                    res += char('0' + digits[idx]);
+                }
+                for (int i = 0; i < idx; i++)
+                    res += char('0' + digits[i]);
+                sort(res.begin(), res.end());
+                reverse(res.begin(), res.end());
+                int zr = 0;
+                for (auto t : res)
+                    zr += (t == '0');
+                res = (zr == res.size() && zr != 0) ? "0" : res;
+                return res;
                 return res;
             }
             else
             {
-               if(mp[2]<2)
-                   return "";
-               else
-               {
-                   int cnt=0;
-                   string res="";
-                   int idx=digits.size()-1;
-                   for(;idx>=0;idx--)
-                   {
-                       if(digits[idx]%3==2)
-                       {
-                           cnt++;
-                           if(cnt==2)
-                               break;
-                       }
-                       else
-                           res+=char(digits[idx]+'0');
-                   }
-                   for(int i=0;i<idx;i++)
-                       res+=char(digits[i]+'0');
-                   sort(res.begin(),res.end());
-                   reverse(res.begin(),res.end());
-                    int zr=0;
-            for(auto t: res)
-                zr+=(t=='0');
-           res=(zr==res.size() && zr!=0)?"0":res;
-            return res;
-                   return res;
-   
-               }
+                if (mp[2] < 2)
+                    return "";
+                else
+                {
+                    int cnt = 0;
+                    string res = "";
+                    int idx = digits.size() - 1;
+                    for (; idx >= 0; idx--)
+                    {
+                        if (digits[idx] % 3 == 2)
+                        {
+                            cnt++;
+                            if (cnt == 2)
+                                break;
+                        }
+                        else
+                            res += char(digits[idx] + '0');
+                    }
+                    for (int i = 0; i < idx; i++)
+                        res += char(digits[i] + '0');
+                    sort(res.begin(), res.end());
+                    reverse(res.begin(), res.end());
+                    int zr = 0;
+                    for (auto t : res)
+                        zr += (t == '0');
+                    res = (zr == res.size() && zr != 0) ? "0" : res;
+                    return res;
+                    return res;
+                }
             }
         }
         else
         {
-            if(mp[2]>=1)
+            if (mp[2] >= 1)
             {
-               string res="";
-                int cnt=0;
-                int idx=digits.size()-1;
-                for(;idx>=0;idx--)
+                string res = "";
+                int cnt = 0;
+                int idx = digits.size() - 1;
+                for (; idx >= 0; idx--)
                 {
-                    if(digits[idx]%3==2)
+                    if (digits[idx] % 3 == 2)
                         break;
-                    res+=char('0'+digits[idx]);
+                    res += char('0' + digits[idx]);
                 }
-                for(int i=0;i<idx;i++)
-                    res+=char('0'+digits[i]);
-                sort(res.begin(),res.end());
-                reverse(res.begin(),res.end());
-                 int zr=0;
-            for(auto t: res)
-                zr+=(t=='0');
-           res=(zr==res.size() && zr!=0)?"0":res;
-            return res;
+                for (int i = 0; i < idx; i++)
+                    res += char('0' + digits[i]);
+                sort(res.begin(), res.end());
+                reverse(res.begin(), res.end());
+                int zr = 0;
+                for (auto t : res)
+                    zr += (t == '0');
+                res = (zr == res.size() && zr != 0) ? "0" : res;
                 return res;
-            
+                return res;
             }
             else
             {
-                if(mp[1]>=2)
+                if (mp[1] >= 2)
                 {
-                    int cnt=0;
-                   string res="";
-                   int idx=digits.size()-1;
-                   for(;idx>=0;idx--)
-                   {
-                       if(digits[idx]%3==1)
-                       {
-                           cnt++;
-                           if(cnt==2)
-                               break;
-                       }
-                       else
-                           res+=char(digits[idx]+'0');
-                   }
-                   for(int i=0;i<idx;i++)
-                       res+=char(digits[i]+'0');
-                   sort(res.begin(),res.end());
-                   reverse(res.begin(),res.end());
-                     int zr=0;
-                for(auto t: res)
-                    zr+=(t=='0');
-                res=(zr==res.size() && zr!=0)?"0":res;
-                return res;
-                   return res;
+                    int cnt = 0;
+                    string res = "";
+                    int idx = digits.size() - 1;
+                    for (; idx >= 0; idx--)
+                    {
+                        if (digits[idx] % 3 == 1)
+                        {
+                            cnt++;
+                            if (cnt == 2)
+                                break;
+                        }
+                        else
+                            res += char(digits[idx] + '0');
+                    }
+                    for (int i = 0; i < idx; i++)
+                        res += char(digits[i] + '0');
+                    sort(res.begin(), res.end());
+                    reverse(res.begin(), res.end());
+                    int zr = 0;
+                    for (auto t : res)
+                        zr += (t == '0');
+                    res = (zr == res.size() && zr != 0) ? "0" : res;
+                    return res;
+                    return res;
                 }
                 else
                     return "";
             }
         }
-        
     }
 };
