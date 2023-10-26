af1f0865acd9000f36b8ab5cfc47f68268eaf3ae
Ankur8789 <109090385+Ankur8789@users.noreply.github.com>
2023-10-27 01:24:54 +0530

    Time: 40 ms (79.92%), Space: 9.6 MB (55.91%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-27-005-data-darkreader-inline-bgcolordivamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4ea8fe-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div823-binary-trees-with-factors/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-27-005-data-darkreader-inline-bgcolordivamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4ea8fe-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div823-binary-trees-with-factors.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-27-005-data-darkreader-inline-bgcolordivamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4ea8fe-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div823-binary-trees-with-factors/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-27-005-data-darkreader-inline-bgcolordivamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4ea8fe-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div823-binary-trees-with-factors.cpp
@@ -1,29 +1,48 @@
 class Solution {
 public:
-     int mod = pow(10,9) + 7;
+    typedef long long ll;
+    const ll mod=1e9+7;
+    unordered_map<ll,ll> mp;
+    ll dp[1001];
+    ll f(ll i,vector<int>& arr)
+    {
+        if(dp[i]!=-1)
+            return dp[i];
+            
+        ll ans=1;
+        for(ll j=0;j<i;j++)
+        {
+            if((arr[i]%arr[j])==0 && mp.find(arr[i]/arr[j])!=mp.end())
+            {
+                // do possible subtrees
+                ll a=j;
+                ll b=mp[arr[i]/arr[j]];
+                ll res=1;
+                // left possible subtrees se ek chune
+                res*=f(a,arr);
+                res%=mod;
+                // right possible se ek chune
+                res*=f(b,arr);
+                res%=mod;
+                // ans=(left C 1)*(right C 1)
+                ans+=res;
+                ans%=mod;
+            }
+        }
+        return dp[i]=ans;
+    }
     int numFactoredBinaryTrees(vector<int>& arr) 
     {
-        sort(arr.begin(), arr.end());
-        unordered_map <int, long> rootWithCount;
-        rootWithCount[arr[0]] = 1;
-        for(int i = 1; i < arr.size(); i++)
+        sort(begin(arr),end(arr));
+        for(ll i=0;i<arr.size();i++)
+            mp[arr[i]]=i;
+        memset(dp,-1,sizeof(dp));
+        ll ans=0;
+        for(ll i=0;i<arr.size();i++)
         {
-            long count = 1;
-            for(auto j : rootWithCount)
-            {
-                int rootEle = j.first;
-                if(arr[i] % rootEle == 0 && rootWithCount.find(arr[i] / rootEle) != rootWithCount.end()){
-                    count += rootWithCount[rootEle] * rootWithCount[arr[i] / rootEle];
-                    
-                }
-            }
-            rootWithCount[arr[i]] = count;
+           ans+=f(i,arr);
+           ans%=mod;
         }
-        int noOfTrees = 0;
-        for(auto i : rootWithCount)
-        {
-            noOfTrees = (noOfTrees + i.second) % mod;
-        }
-        return noOfTrees;
+        return (int)ans;
     }
 };
