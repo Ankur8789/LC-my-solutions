28229dee1ac3b2afebb16303bfcae4ecead1cdfa
Ankur8789 <109090385+Ankur8789@users.noreply.github.com>
2023-11-17 01:21:28 +0530

    Time: 791 ms (20.54%), Space: 249.8 MB (8.65%) - LeetHub

--- 1766-tree-of-coprimes/1766-tree-of-coprimes.cpp
+++ 1766-tree-of-coprimes/1766-tree-of-coprimes.cpp
@@ -1,93 +1,56 @@
-class Solution {
+class Solution
+{
 public:
-    unordered_map<int,vector<int>> adj;
+    unordered_map<int, vector<int>> adj;
     vector<int> ans;
-    unordered_map<int,int> vis;
-    int cnt=0;
+    unordered_map<int, int> vis;
+    int cnt = 0;
     // int memo[51][51];
-    
-    void dfs(int u,int dis,vector<int>& nums,vector<vector<int>>& v)
+
+    void dfs(int u, int dis, vector<int> &nums, vector<vector<int>> &v)
     {
-//         vis[u]=1;
-//         vector<vector<int>> tem=val;
-//         int mini=1e9,kon_hai=1e9;
-//         for(int i=1;i<=50;i++)
-//         {
-//             if(__gcd(i,nums[u])==1)
-//             {
-                
-//                 if(tem[i][0]!=1e9)
-//                 {
-//                     // cout<<i<<" "<<u<<endl;
-//                     if(mini>tem[i][0])
-//                     {
-//                         mini=tem[i][0];
-//                         kon_hai=tem[i][1];
-//                     }
-//                 }
-//             }
-//         }
-//         if(mini!=1e9)
-//         {
-//             // cout<<u<<" "<<kon_hai<<" "<<mini<<endl;
-//             ans[u]=kon_hai;
-//         }
-//         else
-//             ans[u]=-1;
-//         // for(int i=1;i<=50;i++)
-//         // {
-//         //     if(tem[i][0]!=1e9)
-//         //         tem[i][0]++;
-//         // }
-//         tem[nums[u]]={dis,u};
-//         for(auto v: adj[u])
-//         {
-//             if(vis.find(v)==vis.end())
-//                 dfs(v,dis+1,nums,tem);
-//         }
-        vis[u]=1;
-      int val=-1;
-      int ht=INT_MAX;
-      for (int i=1;i<=50;i++)
-      {
-          if (v[i][0]!=1e9 && __gcd(nums[u],i)==1)
-          {
-              if (ht>dis-v[i][0])
-              {
-                  ht=dis-v[i][0];
-                  val=v[i][1];
-              }
-          }
-      }
-      vector<int>temp=v[nums[u]];
-      ans[u]=val;
-      v[nums[u]]={dis,u};
-      for (auto it: adj[u])
-      {
-          if (vis.find(it)==vis.end()) 
-          dfs(it,dis+1,nums,v);
-      }
-      v[nums[u]]=temp;
-    }
-    vector<int> getCoprimes(vector<int>& nums, vector<vector<int>>& ed) 
-    {
-        for(auto t: ed)
+
+        vis[u] = 1;
+        int val = -1;
+        int ht = INT_MAX;
+        for (int i = 1; i <= 50; i++)
         {
-            int u=t[0];
-            int v=t[1];
+            if (v[i][0] != 1e9 && __gcd(nums[u], i) == 1)
+            {
+                if (ht > dis - v[i][0])
+                {
+                    ht = dis - v[i][0];
+                    val = v[i][1];
+                }
+            }
+        }
+        vector<int> temp = v[nums[u]];
+        ans[u] = val;
+        v[nums[u]] = {dis, u};
+        for (auto it : adj[u])
+        {
+            if (vis.find(it) == vis.end())
+                dfs(it, dis + 1, nums, v);
+        }
+        v[nums[u]] = temp;
+    }
+    vector<int> getCoprimes(vector<int> &nums, vector<vector<int>> &ed)
+    {
+        for (auto t : ed)
+        {
+            int u = t[0];
+            int v = t[1];
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
-        int n=nums.size();
+        int n = nums.size();
         ans.resize(n);
-        ans[0]=-1;
+        ans[0] = -1;
         // i , distance , kon hai
-        vector<vector<int>> val(51,vector<int>(2,1e9));
-        dfs(0,0,nums,val);
-        
+        vector<vector<int>> val(51, vector<int>(2, 1e9));
+        dfs(0, 0, nums, val);
+
         // cout<<cnt<<endl;
         return ans;
-        
-        
     }
 };
