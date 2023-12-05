3f32cfdf0cd75fed51736aae7c0491a7a1d49a31
Ankur8789 <109090385+Ankur8789@users.noreply.github.com>
2023-12-05 19:48:45 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (36.15%) - LeetHub

---
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numberOfMatches(int n) 
+    {
+        int ans=0;
+        while(n>1)
+        {
+            ans+=(n/2);
+           if(n&1)
+           {
+               n++;
+               n/=2;
+           }
+           else
+               n/=2;
+        }
+        return ans;
+    }
+};
