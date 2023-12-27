d8b027e3b7559150dca26166eee2988f276e281f
Ankur Kumar <109090385+Ankur8789@users.noreply.github.com>
2023-12-27 19:29:06 +0530

    Time: 330 ms (22.37%), Space: 77.8 MB (95.12%) - LeetHub

--- 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     static bool cmp(vector<int>& a,vector<int>&b)
     {
-        if(a[0]==b[0])
-            return a[1]>b[1];
+        if(a[1]==b[1])
+            return a[0]>b[0];
         else
-            return a[0]<b[0];
+            return a[1]<b[1];
         
     }
     int maxEnvelopes(vector<vector<int>>& env) 
@@ -13,14 +13,14 @@ public:
         sort(env.begin(),env.end(),cmp);
         int ans=1;
         vector<int> val;
-        val.push_back(env[0][1]);
+        val.push_back(env[0][0]);
         for(int i=1;i<env.size();i++)
         {
             int n=val.size();
-            if(env[i][1]>val[n-1])
+            if(env[i][0]>val[n-1])
             {
                 // ans++;
-                val.push_back(env[i][1]);
+                val.push_back(env[i][0]);
             }
             else
             {
@@ -29,7 +29,7 @@ public:
                 while(l<=r)
                 {
                    int mid=(l+r)/2;
-                   if(val[mid]>=env[i][1])
+                   if(val[mid]>=env[i][0])
                    {
                        t=mid;
                        r=mid-1;
@@ -37,7 +37,7 @@ public:
                     else
                        l=mid+1; 
                 }
-                val[t]=env[i][1];
+                val[t]=env[i][0];
             }
         }
             
