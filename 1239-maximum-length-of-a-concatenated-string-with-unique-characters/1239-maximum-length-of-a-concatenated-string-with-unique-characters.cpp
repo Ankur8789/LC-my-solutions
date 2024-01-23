bd77abd76e42be5009e9c0d4822e1b458e15be3a
Ankur Kumar <109090385+Ankur8789@users.noreply.github.com>
2024-01-23 20:45:40 +0530

    Time: 10 ms (82.01%), Space: 14.7 MB (68.36%) - LeetHub

---
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    int max(int a,int b)
+    {
+        if(a>b)
+            return a;
+        else
+            return b;
+    }
+    int f(int i,vector<string>& arr,int last,int st,int mask)
+    {
+        if(i==arr.size())
+            return 0;
+        int ans=0;
+        if(st)
+        {
+           bool ok=true;
+           int tmask=mask;
+             string curr=arr[i];
+           for(auto t: curr)
+           {
+               if((tmask>>(t-'a'))&1)
+               {
+                   ok=false;
+                   break;
+               }
+               else
+                   tmask|=(1<<(t-'a'));
+           }
+            if(ok)
+            ans=max(ans,arr[i].size()+f(i+1,arr,curr.back()-'a',st^1,tmask));
+            ans=max(ans,f(i+1,arr,last,st,mask));
+        }
+        else
+        {
+           string curr=arr[i];
+           string pre=arr[i-1];
+           bool ok=true;
+           int tmask=mask;
+           for(auto t: curr)
+           {
+               if((tmask>>(t-'a'))&1)
+               {
+                   ok=false;
+                   break;
+               }
+               else
+                   tmask|=(1<<(t-'a'));
+           }
+           if(ok)
+               ans=max(ans,curr.size()+f(i+1,arr,curr.back()-'a',st,tmask));
+           ans=max(ans,f(i+1,arr,last,st,mask));
+        }
+        return ans;
+    }
+    int maxLength(vector<string>& arr) 
+    {
+        int ans=f(0,arr,0,1,0);
+        return ans;
+    }
+};
