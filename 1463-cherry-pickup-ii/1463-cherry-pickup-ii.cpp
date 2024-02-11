dcd779da3f62e27f2c4642c711d38ec58d3ed10e
Ankur Kumar <109090385+Ankur8789@users.noreply.github.com>
2024-02-11 12:32:10 +0530

    Time: 57 ms (74.04%), Space: 20 MB (6.66%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
-    //double decker dp...first time this type of question????????
     typedef long long ll;
     ll dp[101][101][101];
     ll f(ll i,ll j1,ll j2,vector<vector<int>>& grid)
     {
         if(j1<0 || j2<0 || j1>=grid[0].size() || j2>=grid[0].size())
-            return 1LL*INT_MIN-1;
-        if(i==grid.size()-1)
+            return -1e18;
+        if(i==grid.size())
+            return 0;
+        auto& X=dp[i][j1][j2];
+        if(X!=-1)
+            return X;
+        ll ans=-1e18;
+        for(ll x=-1;x<2;x++)
         {
-            if(j1==j2)
-                return grid[i][j1];
-            else
-                return grid[i][j1]+grid[i][j2];
-        }
-        if(dp[i][j1][j2]!=-1)
-            return dp[i][j1][j2];
-        ll ans=0;
-        for(ll x=-1;x<=1;x++)
-        {
-            for(ll y=-1;y<=1;y++)
+            for(ll y=-1;y<2;y++)
             {
+                ll nj1=j1+x;
+                ll nj2=j2+y;
                 if(j1==j2)
-                    ans=max(ans,grid[i][j1]+f(i+1,j1+x,j2+y,grid));
+                    ans=max(ans,grid[i][j1]+f(i+1,nj1,nj2,grid));
                 else
-                    ans=max(ans,grid[i][j1]+grid[i][j2]+f(i+1,j1+x,j2+y,grid));
+                    ans=max(ans,grid[i][j1]+grid[i][j2]+f(i+1,nj1,nj2,grid));     
             }
         }
-        return dp[i][j1][j2]=ans;
+        return X=ans;
     }
-    int cherryPickup(vector<vector<int>>& grid)
-    { 
+    int cherryPickup(vector<vector<int>>& grid) 
+    {
         memset(dp,-1,sizeof(dp));
         ll ans=f(0,0,grid[0].size()-1,grid);
         return ans;
