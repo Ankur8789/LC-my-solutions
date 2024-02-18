83133b1bb193852f9cd70c97759d43463f7df2b3
Ankur Kumar <109090385+Ankur8789@users.noreply.github.com>
2024-02-18 11:54:01 +0530

    Time: 119 ms (16.08%), Space: 72 MB (7.11%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
@@ -1,42 +1,32 @@
-class Solution
-{
+class Solution {
 public:
-    int furthestBuilding(vector<int> &heights, int bricks, int ladders)
+    int furthestBuilding(vector<int>& h, int bricks, int ladders)
     {
-        int n = heights.size();
-
-        int b = bricks;
-        int l = ladders;
-
-        priority_queue<int> pq;
-
-        for (int i = 0; i < n - 1; i++)
-        {
-            if (heights[i] > heights[i + 1])
-                continue;
-
-            int diff = heights[i + 1] - heights[i];
-
-            pq.push(diff);
-
-            if (diff <= b)
-            {
-                b -= diff;
-            }
-            else if (diff > b)
-            {
-                if (l > 0)
-                {
-                    int top = pq.top();
-                    pq.pop();
-                    b += top - diff;
-                    --l;
-                }
-                else
-                    return i;
-            }
-        }
-
-        return b >= 0 ? n - 1 : 0;
+         multiset<int> ms;
+         for(int i=0;i<h.size()-1;i++)
+         {
+             if(h[i]>=h[i+1])
+                 continue;
+             else
+             {
+                 int rem=h[i+1]-h[i];
+                 ms.insert(rem);
+                 if(bricks>0 && bricks>=h[i+1]-h[i])
+                 {
+                     bricks-=h[i+1]-h[i];
+                 }
+                 else if(ladders>0)
+                 {
+                     ladders-=1;
+                     auto v=*ms.rbegin();
+                     ms.erase(--ms.end());
+                     bricks+= v-rem;
+                 }
+                 else
+                     return i;
+                     
+             }
+         }
+        return h.size()-1;
     }
 };
