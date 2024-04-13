25f692430eff028726b1d8425ec284f1b3ea83f2
Ankur Kumar <109090385+Ankur8789@users.noreply.github.com>
2024-04-13 10:49:49 +0530

    Time: 27 ms (84.46%), Space: 17.3 MB (85.09%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,59 +1,59 @@
-//largest are histogram solution ...but i dont think it is dp
-class Solution {
+#define fastio()                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+class Solution
+{
 public:
-    int mah(vector<int>& heights) {
+    int mah(vector<int> &heights)
+    {
         int res = 0, curr = 0, n = heights.size();
         stack<int> s;
-        for(int i=0;i<n;i++)
+        for (int i = 0; i < n; i++)
         {
-            while(s.empty() == false && heights[s.top()]>=heights[i]){
+            while (s.empty() == false && heights[s.top()] >= heights[i])
+            {
                 int top = s.top();
                 s.pop();
-                if(s.empty())
-                    curr = heights[top]*i;
+                if (s.empty())
+                    curr = heights[top] * i;
                 else
-                    curr = heights[top]*(i-s.top()-1);
-                res = max(curr,res);
+                    curr = heights[top] * (i - s.top() - 1);
+                res = max(curr, res);
             }
             s.push(i);
         }
-       
-        while(s.empty() == false)
+
+        while (s.empty() == false)
         {
-                int top = s.top();
-                s.pop();
-                if(s.empty())
-                    curr = heights[top]*n;
-                else
-                    curr = heights[top]*(n-s.top()-1);
-                res = max(curr,res);
-            }
+            int top = s.top();
+            s.pop();
+            if (s.empty())
+                curr = heights[top] * n;
+            else
+                curr = heights[top] * (n - s.top() - 1);
+            res = max(curr, res);
+        }
         return res;
     }
-    
-    
-    
-    
-    int maximalRectangle(vector<vector<char>>& matrix) 
+
+    int maximalRectangle(vector<vector<char>> &matrix)
     {
-        int n=matrix.size();
-        int m=matrix[0].size();
-        int ans=-1e5;
+        fastio();
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int ans = -1e5;
         vector<int> hs(m);
-        for(int i=0;i<n;i++)
+        for (int i = 0; i < n; i++)
         {
-            for(int j=0;j<m;j++)
+            for (int j = 0; j < m; j++)
             {
-                if(matrix[i][j]=='0')
-                    hs[j]=0;
+                if (matrix[i][j] == '0')
+                    hs[j] = 0;
                 else
-                    hs[j]+=1;
+                    hs[j] += 1;
             }
-            for(auto t: hs)cout<<t<<" ";
-            cout<<endl;
-            ans=max(ans,mah(hs));
-            
-            
+            ans = max(ans, mah(hs));
         }
         return ans;
     }
