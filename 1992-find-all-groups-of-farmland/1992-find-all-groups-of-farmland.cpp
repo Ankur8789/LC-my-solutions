cc9cafd8853497ef7f728c35a68f8e8f2e45714b
Ankur Kumar <109090385+Ankur8789@users.noreply.github.com>
2024-04-20 19:00:42 +0530

    Time: 101 ms (72.00%), Space: 63.8 MB (76.36%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
@@ -1,41 +1,47 @@
 class Solution {
 public:
-    int li=-1,ri=-1,le=-1,re=-1;
-    int vis[301][301];
-    void dfs(int i,int j,vector<vector<int>>& land)
+    vector<vector<int>> vis;
+    int tx, ty , bx , by;
+    void dfs(int i , int j, vector<vector<int>>& land)
     {
-        vis[i][j]=1;
-        if(i>le)
+        if(i >= land.size() || j >= land[0].size())
+            return ;
+        if(land[i][j] == 0 || vis[i][j] == 1)
+            return ;
+        vis[i][j] = 1;
+        if(i > bx)
         {
-            le=i;
-            re=j;
+            bx = i;
+            by = j;
         }
-        else if(le==i)
-            re=max(re,j);
-        if(i+1<land.size() && land[i+1][j]==1 && vis[i+1][j]==0)
-            dfs(i+1,j,land);
-        if(j+1<land[0].size() && land[i][j+1]==1 && vis[i][j+1]==0)
-            dfs(i,j+1,land);
+        else if(i == bx)
+            by = max(by , j);
+        dfs(i+1,  j ,land);
+        dfs(i , j+1, land);
+        
     }
     vector<vector<int>> findFarmland(vector<vector<int>>& land) 
     {
-        int m=land.size();
-        int n=land[0].size();
-        vector<vector<int>> ans;
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(vis[i][j]==0 && land[i][j]==1)
-                {
-                    li=i,ri=j;
-                    le=i,re=j;
-                    dfs(i,j,land);
-                    ans.push_back({li,ri,le,re});
-                    li=-1,ri=-1,le=-1,re=-1;
-                }
-            }
-        }
-        return ans;
+       int m = land.size();  
+       int n = land[0].size();
+       vis.resize(m,vector<int>(n , 0));
+       vector<vector<int>> ans;
+       for(int i = 0; i < m ; i++)
+       {
+          for(int j = 0; j < n ;j++)
+          {
+             if(vis[i][j] == 0 && land[i][j] == 1)
+             {
+                 tx = i;
+                 ty = j;
+                 bx = i;
+                 by = j;
+                 dfs(i , j , land);
+                 ans.push_back({tx , ty , bx , by});
+             }
+          }
+       }
+       return ans;
+       
     }
 };
