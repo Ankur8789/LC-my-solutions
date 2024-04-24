16614bbe1e52cee345532b12d9385791b8411cfb
Ankur Kumar <109090385+Ankur8789@users.noreply.github.com>
2024-04-24 22:37:50 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (29.12%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-    int dp [38];
-    int f(int n)
-    {
-        
-        if(n == 0)
-            return 0;
-        if(n == 1 || n == 2)
-            return 1;
-        auto & x = dp[n];
-        if(x != -1)
-            return x;
-        return  x = f(n-1) + f(n-2) + f(n-3);
-    }
+   
     int tribonacci(int n) 
     {
-       memset(dp , -1 , sizeof(dp));
-       int ans = f(n);
-       return ans;
+        if(n == 0)return 0;
+        if(n == 1 || n == 2)return 1;
+        int dp[n+1];
+        dp[0] = 0;
+        dp[1] = dp[2] = 1;
+        for(int i = 3;i <= n;i++)
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        return dp[n];
     }
 };
