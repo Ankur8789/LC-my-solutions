38f417cdc3e82fe2b9823c894e9cfc792b932757
Ankur Kumar <109090385+Ankur8789@users.noreply.github.com>
2024-04-27 17:25:46 +0530

    Time: 19 ms (59.67%), Space: 13.9 MB (67.69%) - LeetHub

--- 0514-freedom-trail/0514-freedom-trail.cpp
+++ 0514-freedom-trail/0514-freedom-trail.cpp
@@ -1,38 +1,39 @@
-class Solution {
+class Solution
+{
 public:
     int dp[101][101];
-    int f(int i,int curr,string& ring,string& key,map<char,vector<int>>& mp)
+    int f(int i, int curr, string &ring, string &key, map<char, vector<int>> &mp)
     {
-        if(curr==key.size())
+        if (curr == key.size())
             return 0;
-        auto&x =dp[i][curr];
-        if(x!=-1)
+        auto &x = dp[i][curr];
+        if (x != -1)
             return x;
-        int ans=1e9;
+        int ans = 1e9;
         vector<int> temp = mp[key[curr]];
-        for(int idx=0;idx<temp.size();idx++)
+        for (int idx = 0; idx < temp.size(); idx++)
         {
             int val1 = abs(i - temp[idx]);
             int val2 = ring.size() - val1;
-            ans = min(ans,val1+f(temp[idx],curr+1,ring,key,mp));
-            ans = min(ans,val2+f(temp[idx],curr+1,ring,key,mp)); 
+            ans = min(ans, val1 + f(temp[idx], curr + 1, ring, key, mp));
+            ans = min(ans, val2 + f(temp[idx], curr + 1, ring, key, mp));
         }
-        return  x=ans;
+        return x = ans;
     }
-    int findRotateSteps(string ring, string key) 
+    int findRotateSteps(string ring, string key)
     {
-        map<char,vector<int>> mp;
-        for(int i=0;i<ring.size();i++)
-        mp[ring[i]].push_back(i);
+        map<char, vector<int>> mp;
+        for (int i = 0; i < ring.size(); i++)
+            mp[ring[i]].push_back(i);
         int ans = 1e9;
         vector<int> temp = mp[key[0]];
-        memset(dp,-1,sizeof(dp));
-        for(int idx=0;idx<temp.size();idx++)
+        memset(dp, -1, sizeof(dp));
+        for (int idx = 0; idx < temp.size(); idx++)
         {
             int val1 = abs(0 - temp[idx]);
             int val2 = ring.size() - val1;
-            ans = min(ans,val1+f(temp[idx],0+1,ring,key,mp));
-            ans = min(ans,val2+f(temp[idx],0+1,ring,key,mp)); 
+            ans = min(ans, val1 + f(temp[idx], 0 + 1, ring, key, mp));
+            ans = min(ans, val2 + f(temp[idx], 0 + 1, ring, key, mp));
         }
         return ans + key.size();
     }
