2d6e01419ed76b7c9e21c62429410330df0c2eef
Ankur Kumar <109090385+Ankur8789@users.noreply.github.com>
2024-05-04 12:29:50 +0530

    Time: 55 ms (71.98%), Space: 45.4 MB (46.86%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.cpp
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -2,29 +2,31 @@ class Solution {
 public:
     int numRescueBoats(vector<int>& pe, int lt) 
     {
-        sort(pe.begin(),pe.end());
-        int i=0,j=pe.size()-1;
-        int cc=0;
-        while(i<=j)
-        {
-            if(i==j)
-            {
-               cc++;
+       sort(pe.begin() , pe.end());
+       int cnt = 0;
+       int i = 0, j = 0;
+       int n = pe.size();
+       j = n-1;
+       while(i <= j)
+       {
+           if(i == j)
+           {
+               cnt++;
                break;
-            }
-            if(pe[j]+pe[i]>lt)
-            {
-                cc++;
-                j--;
-            }
-            else
-            {
-                cc++;
-                i++;
-                j--;
-            }
-            
-        }
-        return cc;
+           }
+           if(pe[i] + pe[j] <= lt)
+           {
+               cnt++;
+               i++;
+               j--;
+           }
+           else
+           {
+               cnt++;
+               j--;
+           }
+       }
+        return cnt;
+       
     }
 };
