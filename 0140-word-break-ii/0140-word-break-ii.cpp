52fe15c8ab2f4a97bbe4abf9c90333976185b45b
Ankur Kumar <109090385+Ankur8789@users.noreply.github.com>
2024-05-25 17:12:26 +0530

    Time: 4 ms (32.03%), Space: 9 MB (57.79%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.cpp
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -1,41 +1,34 @@
 class Solution {
 public:
     vector<string> ans;
-    void f(int i,string s,unordered_set<string>& st,string& str)
+    void f(int i,string& s,unordered_set<string>& hsh,string x)
     {
-        if(i==s.length())
+        if(i==s.size())
         {
-            ans.push_back(str);
-            return ;
+            ans.push_back(x);
+            return;
         }
-        string temp="";
-        if(str.size()!=0)
-        str+=' ';
-        int cnt=0;
-        
+        string str="";
         for(int idx=i;idx<s.size();idx++)
         {
-            temp+=s[idx];
-            str+=s[idx];
-            //cout<<str<<endl;
-            cnt++;
-            if(st.find(temp)==st.end())
-                continue;
-            //str+=' ';
-            f(idx+1,s,st,str);
-           
+           str+=s[idx];
+           if(hsh.find(str)!=hsh.end())
+           {
+               if(i==0)
+                   f(idx+1,s,hsh,str);
+               else
+                   f(idx+1,s,hsh,x+' '+str);
+           }
         }
-        for(int j=0;j<=cnt;j++)
-        str.pop_back();
+            
     }
     vector<string> wordBreak(string s, vector<string>& wordDict) 
     {
-        unordered_set<string> st;
-        for(auto t:wordDict)
-            st.insert(t);
-        string str="";
-        f(0,s,st,str);
-        return ans;
-        
+      unordered_set<string> hsh;
+      for(auto x: wordDict)
+          hsh.insert(x);
+      ans.clear();
+      f(0,s,hsh,"");
+      return ans;
     }
 };
