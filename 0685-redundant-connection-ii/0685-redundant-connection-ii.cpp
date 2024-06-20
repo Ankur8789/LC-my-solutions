1e2843adf41b7f4867c742355775eece88c2391f
Ankur Kumar <109090385+Ankur8789@users.noreply.github.com>
2024-06-20 23:15:21 +0530

    Time: 137 ms (8.61%), Space: 91.3 MB (5.43%) - LeetHub

--- 0685-redundant-connection-ii/0685-redundant-connection-ii.cpp
+++ 0685-redundant-connection-ii/0685-redundant-connection-ii.cpp
@@ -1,62 +1,69 @@
 class Solution {
 public:
-    vector<int> findRedundantDirectedConnection(vector<vector<int>>& edges) 
+    vector<int> findRedundantDirectedConnection(vector<vector<int>>& edges)
     {
-        int n=edges.size();
-        unordered_map<int,vector<int>> adj;
-        vector<int> ideg(n+1,0);
-        for(auto t: edges)
-        {
-            adj[t[0]].push_back(t[1]);
-            ideg[t[1]]++;
-        }
-        vector<int> ans;
-        for(auto t: edges)
-        {
-            ideg[t[1]]--;
-            vector<int> vis(n+1,0);
-            queue<int> q;
-            bool con=true;
-            for(int i=1;i<=n;i++)
-            {
-                if(ideg[i]==0)
-                {
-                   q.push(i); 
-                    vis[i]=1;
-                    break;
-                }
-            }
-            while(q.size()>0)
-            {
-                auto u=q.front();
-                q.pop();
-                for(auto v: adj[u])
-                {
-                    if(t[0]==u && t[1]==v)
-                        continue;
-                    if(vis[v]==1)
-                    {
-                        con=false;
-                        break;
-                    }
-                    vis[v]=1;
-                    q.push(v);
-                     
-                }
-            }
-             ideg[t[1]]++;
-             for(int i=1;i<=n;i++)
-             {
-                 if(vis[i]==0)
-                 {
-                     con=false;
-                     break;
-                 }
-             }
-            if(con)
-                ans=t;
-            
-        }
+       unordered_map<int,vector<int>> adj;
+       int n=edges.size();
+       vector<int> ideg(n+1);
+       vector<int> ans;
+       for(auto x: edges)
+       {
+           adj[x[0]].push_back(x[1]);
+           ideg[x[1]]+=1;
+       }
+       for(auto x: edges)
+       {
+           // remove this edge
+           ideg[x[1]]--;
+           queue<int> q;
+           vector<int> vis(n+1,0);
+           for(int i=1;i<=n;i++)
+           {
+               if(ideg[i]==0)
+               {
+                   q.push(i);
+                   vis[i]=1;
+                   break;
+               }
+           }
+           vector<int> tmpideg=ideg;
+           while(q.size())
+           {
+               auto y=q.front();
+               q.pop();
+               int u=y;
+               
+               for(auto v: adj[u])
+               {
+                   if(u==x[0] && v==x[1])
+                       continue;
+                   else
+                   {
+                       if(vis[v]==0)
+                       {
+                           tmpideg[v]--;
+                           if(tmpideg[v]==0)
+                               q.push(v),vis[v]=1;
+                       }
+                   }
+               }
+           }
+           bool ok=true;
+           for(int i=1;i<=n;i++)
+           {
+               if(vis[i]==0)
+               {
+                   ok=false;
+                   break;
+               }
+           }
+           if(ok)
+               ans=x;
+           ideg[x[1]]++;
+           
+       }
         return ans;
+       
+       
     }
 };
