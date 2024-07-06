72a71d0131fb1d73244fd65acf13ca76b728cd29
Ankur Kumar <109090385+Ankur8789@users.noreply.github.com>
2024-07-06 10:51:58 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (30.58%) - LeetHub

--- 2582-pass-the-pillow/2582-pass-the-pillow.cpp
+++ 2582-pass-the-pillow/2582-pass-the-pillow.cpp
@@ -2,23 +2,7 @@ class Solution {
 public:
     int passThePillow(int n, int time) 
     {
-       int x=0;
-        for(int i=1;i<=n;i++)
-        {
-            if(time==x)
-                return i;
-            x++;
-            if(i==n)
-            {
-                for(int j=n-1;j>=2;j--)
-                {
-                    if(time==x)
-                        return j;
-                    x++;
-                }
-                i=0;
-            }
-        }
-        return 0;
+      time = time % ((n - 1) * 2) + 1;
+      return min(time, n * 2 - time);
     }
 };
