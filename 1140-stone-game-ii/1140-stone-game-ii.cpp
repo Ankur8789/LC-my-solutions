3d2a7014c8c0bf7251188c0c6762eea64348f602
Ankur Kumar <109090385+Ankur8789@users.noreply.github.com>
2024-08-20 21:18:42 +0530

    Time: 14 ms (70.55%), Space: 10.3 MB (90.92%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.cpp
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -1,47 +1,34 @@
 class Solution {
 public:
-    int dp[101][2][101];
-    int f(int i,int turn,vector<int>& piles,int m)
-    {
-        if(i>=piles.size())
+    int dp[101][2][201];
+    int f(int i,int turn,int m,vector<int>& piles){
+        if(i==piles.size())
             return 0;
-        if(dp[i][turn][m]!=-1)
-            return dp[i][turn][m];
-            
-        if(turn)
-        {
-            int ps=0;
-            int val=0;
-            int ans=0;
-            for(int idx=i;idx<i+2*m;idx++)
-            {
-                if(idx==piles.size())
-                    break;
-                ps+=piles[idx];
-                val++;
-                ans=max(ans,ps+f(idx+1,turn^1,piles,max(val,m)));
+        auto&x=dp[i][turn][m];
+        if(x!=-1)
+            return x;
+        int ans=0,n=piles.size();
+        if(turn){
+            int tk=0,sum=0;
+            for(int idx=i;idx<min(n,i+2*m);idx++){
+                tk++;
+                sum+=piles[idx];
+                ans=max(ans,sum+f(idx+1,turn^1,max(m,tk),piles));
             }
-            return dp[i][turn][m]= ans;
         }
-        else
-        {
-            int ans=INT_MAX;
-            int val=0;
-            for(int idx=i;idx<i+2*m;idx++)
-            {
-                if(idx==piles.size())
-                    break;
-                val++;
-                ans=min(ans,f(idx+1,turn^1,piles,max(val,m)));
+        else{
+            int tk=0;
+            ans=1e9;
+            for(int idx=i;idx<min(n,i+2*m);idx++){
+                tk++;
+                ans=min(ans,f(idx+1,turn^1,max(m,tk),piles));
             }
-             return dp[i][turn][m]=ans;
         }
-       
+        return x=ans;
     }
-    int stoneGameII(vector<int>& piles) 
-    {
+    int stoneGameII(vector<int>& piles) {
         memset(dp,-1,sizeof(dp));
-        int ans=f(0,1,piles,1);
+        int ans=f(0,1,1,piles);
         return ans;
     }
 };
