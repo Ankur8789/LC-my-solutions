9581a419956281ec161f90fd85bbd573fc3e514e
Ankur Kumar <109090385+Ankur8789@users.noreply.github.com>
2024-10-05 12:53:43 +0530

    Time: 4 ms (81.50%), Space: 8.7 MB (80.36%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
-    bool checkInclusion(string s1, string s2)
-    {
-        vector<int> a(26);bool ans=false;
-        vector<int> b(26);
-         for(int i=0;i<s1.length();i++)
-          a[int(s1[i])-'a']++;
-        int i=0,j=0;
-        while(j<s2.length())
-        {
-            b[int(s2[j])-'a']++;
-            if(j-i+1<s1.length())
+    bool checkInclusion(string s1, string s2) {
+        vector<int> fre(26);
+        for(auto x: s1)
+            fre[x-'a']+=1;
+        vector<int> tmp(26);
+        int i=0,j=0,n=s2.size(),m=s1.size();
+        while(j<n){
+            tmp[s2[j]-'a']+=1;
+            if(j-i+1<m)
                 j++;
-            else if(j-i+1==s1.length())
-            {
-                 if(a==b)
-                 ans=true;
-                 b[int(s2[i])-'a']--;
-                 i++;j++;
-                if(ans==true)
-                    break;
-                     
+            else if(j-i+1==m){
+                if(tmp==fre)
+                    return 1;
+                tmp[s2[i]-'a']--;
+                i++;
+                j++;
             }
         }
-        return ans;
+        return 0;
     }
 };
