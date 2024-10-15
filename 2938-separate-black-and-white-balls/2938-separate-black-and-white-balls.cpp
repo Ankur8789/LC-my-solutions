903ece53e0cf7cab84fa6e96da5bcf437ab544d7
Ankur Kumar <109090385+Ankur8789@users.noreply.github.com>
2024-10-15 18:51:48 +0530

    Time: 44 ms (6.95%), Space: 31 MB (6.32%) - LeetHub

--- 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
     typedef long long ll;
-    long long minimumSteps(string s) 
-    {
-       vector<ll> ones;
-       for(ll i=0;i<s.size();i++)
-       {
-           if(s[i]=='1')
-               ones.push_back(i);
-       }
-       if(ones.size()==0)
-           return 0;
-       ll ans=0;
-       // sort(ones.begin(),ones.end());
-        reverse(ones.begin(),ones.end());
-       ll n=s.size();
-       if(ones[0]!=n-1)
-           ans+=(n-1-ones[0]);
-       ll curr=n-2;
-       for(ll i=1;i<ones.size();i++)
-       {
-           ans+=abs(curr-ones[i]);
-           curr--;
-       }
-       return ans;
-           
-       
+    long long minimumSteps(string s) {
+        ll n = s.size();
+        vector<ll> ones;
+        for(int i=0;i<n;i++){
+            if(s[i] == '1')
+                ones.push_back(i);
+        }
+        if(ones.size() == 0)
+            return 0;
+        ll ans = 0;
+        ans += n-1 - ones.back();
+        ll curr = n-2;
+        for(ll i=ones.size()-2;i>=0;i--){
+            ans += curr - ones[i];
+            curr--;
+        }
+        return ans;
     }
 };
