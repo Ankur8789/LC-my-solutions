3c20b1db821e0ee474163741d23af018e23bba35
Ankur Kumar <109090385+Ankur8789@users.noreply.github.com>
2024-10-20 21:25:39 +0530

    Time: 3 ms (94.42%), Space: 9.6 MB (75.94%) - LeetHub

---
+++ 1106-parsing-a-boolean-expression/1106-parsing-a-boolean-expression.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    bool parseBoolExpr(string exp) {
+        stack<char> action,type;
+        int n = exp.size();
+        int i = 0;
+        while(i < n){
+            if(exp[i] == '|' || exp[i] == '&' || exp[i] == '!')
+                action.push(exp[i]);
+            else if(exp[i] == 'f' || exp[i] == 't' || exp[i] == ',' || exp[i] == '('){
+                if(exp[i] !=',')
+                    type.push(exp[i]);
+            }
+            else if(exp[i] == ')'){
+                int tr = 0,fl = 0;
+                while(type.size() && type.top()!='('){
+                    if(type.top() == 'f')
+                        fl++;
+                    else
+                        tr++;
+                    type.pop();
+                }
+                type.pop();
+                char opr = action.top();
+                action.pop();
+                if(opr == '|'){
+                    if(tr)
+                        type.push('t');
+                    else
+                        type.push('f');
+                }
+                else if(opr == '!'){
+                    if(tr)
+                        type.push('f');
+                    else
+                        type.push('t');
+                }
+                else{
+                    if(fl)
+                        type.push('f');
+                    else
+                        type.push('t');
+                }
+            }
+            i++;
+        }
+        // while(action.size()){
+        //     cout<<action.top()<<" ";
+        //     action.pop();
+        // }
+        // cout<<endl;
+        // while(type.size()){
+        //     cout<<type.top()<<" ";
+        //     type.pop();
+        // }
+        return (type.top()=='f')?0:1;
+    }
+};
