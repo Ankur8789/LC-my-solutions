cb4d61d53c2ade5a3787383de58732b0aa945362
Ankur Kumar <109090385+Ankur8789@users.noreply.github.com>
2024-11-09 15:37:30 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (98.95%) - LeetHub

---
+++ 3133-minimum-array-end/3133-minimum-array-end.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    typedef long long ll;
+    long long minEnd(int n, int x) {
+       ll ans = 1e18;
+       ll val = x;
+       n--;
+       x = ll(x) , n = ll(n);
+       for(ll i=0;i<64;i++){
+           if(n == 0)
+               break;
+           if((val>>i)&1LL) continue;
+           val |= (n&1LL)<<i;
+           n = n >> 1;
+       }
+       return val;
+    }
+};
