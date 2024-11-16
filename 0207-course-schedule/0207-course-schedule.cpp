be7a6112ac4d03d39a7bf7ec2631d093937ba918
Ankur Kumar <109090385+Ankur8789@users.noreply.github.com>
2024-11-16 22:17:58 +0530

    Time: 2 ms (84.24%), Space: 20.3 MB (23.56%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
-    bool canFinish(int nc, vector<vector<int>>& pre) 
-    {
+    bool canFinish(int n, vector<vector<int>>& pre) {
+        vector<int> ideg(n);
         unordered_map<int,vector<int>> adj;
-        vector<int> ideg(nc,0);
-        for(auto t: pre)
-        {
-            adj[t[1]].push_back(t[0]);
-            ideg[t[0]]++;
+        for(auto x:pre){
+            int v=x[1],u=x[0];
+            adj[v].push_back(u);
+            ideg[u]++;
         }
         queue<int> q;
-        int cc=0;
-        for(int i=0;i<nc;i++)
-        {
-            if(ideg[i]==0)
+        for(int i=0;i<n;i++){
+            if(ideg[i]==0){
                 q.push(i);
-        }
-        while(q.size()>0)
-        {
-            auto t=q.front();
-            q.pop();
-            cc++;
-            for(auto x: adj[t])
-            {
-                ideg[x]--;
-                if(ideg[x]==0)
-                    q.push(x);
             }
         }
-        return cc==nc;
+        int cnt=0;
+        while(q.size()){
+            auto x=q.front();
+            q.pop();
+            cnt++;
+            for(auto v: adj[x]){
+                ideg[v]--;
+                if(ideg[v]==0){
+                    q.push(v);
+                }
+            }
+        }
+        return cnt==n;
     }
 };
