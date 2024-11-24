dfaa56b749a5ded307ba1c410a706a348c5d89b1
Ankur Kumar <109090385+Ankur8789@users.noreply.github.com>
2024-11-24 17:02:28 +0530

    Time: 44 ms (33.38%), Space: 84.4 MB (33.04%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,41 +1,44 @@
 class Solution {
 public:
-    int largestRectangleArea(vector<int>& h) 
-    {
-       stack<int> st;
-       int n = h.size();
-       vector<int> left(n),right(n);
-       for(int i = 0;i < n ;i++)
-       {
-           left[i]=i;
-           while(st.size()>0 && h[st.top()]>=h[i])
+    int largestRectangleArea(vector<int>& h) {
+        int n=h.size();
+        vector<int> left(n),right(n);
+        left[0]=-1,right[n-1]=n;
+        stack<int> st;
+        st.push(0);
+        for(int i=1;i<n;i++){
+            while(st.size()>0 && h[st.top()]>=h[i]){
                 st.pop();
-           if(st.size()==0)
-               left[i] = -1;
-           else
-               left[i]=st.top();
-           st.push(i);
-       }
-       while(st.size())
-           st.pop();
-       for(int i=n-1;i>=0;i--)
-       {
-           right[i] = i;
-           while(st.size()>0 && h[st.top()]>=h[i])
-               st.pop();
-           if(st.size()==0)
-               right[i]=n;
-           else
-               right[i] = st.top();
-           st.push(i);
-       }
-       int maxi=0;
-       for(int i=0;i<n;i++)
-       {
-           int dis = right[i] - left[i] - 1;
-           // cout<<left[i]<<" "<<right[i]<<endl;
-           maxi = max(maxi , dis*h[i]);
-       }
-      return maxi ;
+            }
+            if(st.size()==0){
+                left[i]=-1;
+            }
+            else{
+                left[i]=st.top();
+            }
+            st.push(i);
+        }
+        while(st.size())
+            st.pop();
+        st.push(n-1);
+        for(int i=n-2;i>=0;i--){
+            while(st.size()>0 && h[st.top()]>=h[i]){
+                st.pop();
+            }
+            if(st.size()==0){
+                right[i]=n;
+            }
+            else{
+                right[i]=st.top();
+            }
+            st.push(i);
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+          int l=left[i]+1,r=right[i]-1;
+          int dis=r-l+1;
+          ans=max(ans,h[i]*dis);
+        }
+        return ans;
     }
 };
