0b3619eceb7bc947d72dc55550b094df6aa792bc
Ankur Kumar <109090385+Ankur8789@users.noreply.github.com>
2024-11-24 15:02:36 +0530

    Time: 7 ms (7.84%), Space: 26.6 MB (5.07%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,51 +1,40 @@
 class Solution {
 public:
-    int trap(vector<int>& height) 
-    {
-        int n=height.size();
+    int trap(vector<int>& h) {
+        int n=h.size();
         vector<int> left(n),right(n);
+        left[0]=-1,right[n-1]=-1;
         stack<int> st;
-        right[n-1]=0;
-        right[0]=0;
-        left[0]=0;
-        left[n-1]=0;
-        st.push(height[n-1]);
-        for(int i=n-2;i>0;i--)
-        {
-            while(st.size()>0 && st.top()<height[i])
+        st.push(h[0]);
+        for(int i=1;i<n;++i){
+            while(st.size() && st.top()<h[i])
                 st.pop();
             if(st.size()==0)
-            {
-                right[i]=0;
-                st.push(height[i]);
-            }
+               { left[i]=-1,st.push(h[i]);}
             else
-            {
-                right[i]=st.top();
-            }
+                left[i]=st.top();
+            
         }
         while(st.size())
             st.pop();
-        st.push(height[0]);
-        for(int i=1;i<n-1;i++)
-        {
-            while(st.size()>0 && st.top()<height[i])
+        st.push(h[n-1]);
+        for(int i=n-2;i>=0;i--){
+            while(st.size() && st.top()<h[i])
                 st.pop();
             if(st.size()==0)
-            {
-                left[i]=0;
-                st.push(height[i]);
-            }
+                {right[i]=-1,st.push(h[i]);}
             else
-                left[i]=st.top();
+                right[i]=st.top();
+            
         }
-        int ans=0;
-        for(int i=1;i<n-1;i++)
-        {
-            if(min(left[i],right[i])!=0)
-                ans+=min(left[i],right[i])-height[i];
+        int tot=0;
+        for(int i=0;i<n;i++){
+            int mini=min(left[i],right[i]);
+            if(mini==-1)
+                continue;
+            else 
+                tot+=(mini-h[i]);
         }
-        return ans;
-        
+        return tot;
     }
 };
