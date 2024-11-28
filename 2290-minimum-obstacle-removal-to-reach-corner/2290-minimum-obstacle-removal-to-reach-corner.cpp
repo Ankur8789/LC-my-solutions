7ceb1db5dee1b8862641f432d605ad9781f8473f
Ankur Kumar <109090385+Ankur8789@users.noreply.github.com>
2024-11-28 22:49:01 +0530

    Time: 1371 ms (9.57%), Space: 437.1 MB (8.98%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
@@ -1,59 +1,41 @@
 class Solution {
 public:
-    vector<pair<int,int>> dirs={{0,-1},{0,1},{1,0},{-1,0}};
-    bool check(int x,int y,vector<vector<int>>& grid)
-    {
-        if(x<0 || y<0 || x>=grid.size() || y>=grid[0].size())
+    vector<pair<int,int>> dirs={{0,1},{0,-1},{-1,0},{1,0}};
+    bool check(int x,int y,vector<vector<int>>& grid){
+        if(x>=grid.size() || y>=grid[0].size() || x<0 ||y<0)
             return 0;
         return 1;
     }
-    int minimumObstacles(vector<vector<int>>& grid) 
-    {
+    int minimumObstacles(vector<vector<int>>& grid) {
         multiset<vector<int>> ms;
-        int m=grid.size();
-        int n=grid[0].size();
-        vector<vector<int>> dis(m,vector<int>(n,1e9));
-        if(grid[0][0]==0)
-            dis[0][0]=0;
-        else
-            dis[0][0]=1;
-        ms.insert({dis[0][0],0,0});
-        while(ms.size()>0)
-        {
-            auto t=*ms.begin();
+        int m=grid.size(),n=grid[0].size();
+        int dis[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                dis[i][j]=1e9;
+            }
+        }
+        dis[0][0]=0;
+        ms.insert({0,0,0});
+        // d x y
+        while(ms.size()){
+            auto x=*ms.begin();
             ms.erase(ms.begin());
-            int i=t[1];
-            int j=t[2];
-            int dist=t[0];
-            for(auto p:dirs)
-            {
-                int x=i+p.first;
-                int y=j+p.second;
-                if(check(x,y,grid))
-                {
-                    if(grid[x][y]==1)
-                    {
-                       if(dist+1<dis[x][y])
-                       {
-                           if(ms.count({dis[x][y],x,y}))
-                               ms.erase({dis[x][y],x,y});
-                           dis[x][y]=dist+1;
-                           ms.insert({dis[x][y],x,y});
-                       }
-                    }
-                    else
-                    {
-                        if(dist<dis[x][y])
-                        {
-                            if(ms.count({dis[x][y],x,y}))
-                               ms.erase({dis[x][y],x,y});
-                           dis[x][y]=dist;
-                           ms.insert({dis[x][y],x,y});
-                        }
+            int d=x[0],i=x[1],j=x[2];
+            for(auto t: dirs){
+                int nx=i+t.first,ny=j+t.second;
+                if(check(nx,ny,grid)){
+                    int p=0;
+                    if(grid[nx][ny]==1)
+                        p++;
+                    if(dis[nx][ny]>d+p){
+                        if(ms.count({dis[nx][ny],nx,ny}))
+                            ms.erase({dis[nx][ny],nx,ny});
+                        dis[nx][ny]=d+p;
+                        ms.insert({dis[nx][ny],nx,ny});
                     }
                 }
             }
-            
         }
         return dis[m-1][n-1];
     }
