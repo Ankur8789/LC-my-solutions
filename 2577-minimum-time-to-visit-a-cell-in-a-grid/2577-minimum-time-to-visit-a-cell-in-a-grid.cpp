bb07d3ac0213657fe627cd15429cb7deed879c85
Ankur Kumar <109090385+Ankur8789@users.noreply.github.com>
2024-11-29 17:24:34 +0530

    Time: 1284 ms (5.15%), Space: 332.3 MB (5.66%) - LeetHub

--- 2577-minimum-time-to-visit-a-cell-in-a-grid/2577-minimum-time-to-visit-a-cell-in-a-grid.cpp
+++ 2577-minimum-time-to-visit-a-cell-in-a-grid/2577-minimum-time-to-visit-a-cell-in-a-grid.cpp
@@ -1,74 +1,48 @@
 class Solution {
 public:
-    vector<pair<int,int>> dir={{-1,0},{1,0},{0,-1},{0,1}};
-    int m,n;
-    bool check(int x,int y)
-    {
+    vector<vector<int>> dirs={{0,1},{0,-1},{1,0},{-1,0}};
+    bool check(int x,int y,int m,int n){
         if(x<0 || y<0 || x>=m || y>=n)
-            return false;
-        return true;
+            return 0;
+        return 1;
     }
-    int minimumTime(vector<vector<int>>& grid) 
-    {
-        m=grid.size();
-        n=grid[0].size();
-        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>> q;
-        if(grid[0][1]>grid[0][0]+1 && grid[1][0]>1)
+    int minimumTime(vector<vector<int>>& grid) {
+         if(grid[0][1]>grid[0][0]+1 && grid[1][0]>1)
             return -1;
+        int m=grid.size(),n=grid[0].size();
         vector<vector<int>> dis(m,vector<int>(n,1e9));
         dis[0][0]=0;
-        q.push({0,0,0});
-        while(q.size()>0)
-        {
-            auto x=q.top();
-            int dist=x[0];
-            // int pari=x[1];
-            // int parj=x[2];
-            int i=x[1];
-            int j=x[2];
-            q.pop();
-            for(auto t: dir)
-            {
-                int ni=i+t.first;
-                int nj=j+t.second;
-                if(check(ni,nj))
-                {
-                    if(dist+1>=grid[ni][nj])
-                    {
-                        if(dist+1<dis[ni][nj])
-                        {
-                           dis[ni][nj]=dist+1;
-                           q.push({dist+1,ni,nj});
-                        }
-                        else
-                        {
-                            continue;
+        multiset<vector<int>> ms;
+        ms.insert({dis[0][0],0,0});
+        while(ms.size()){
+            auto p=*ms.begin();
+            ms.erase(ms.begin());
+            int d=p[0],x=p[1],y=p[2];
+            bool move=false;
+            for(auto t: dirs){
+                int nx=x+t[0],ny=y+t[1];
+                if(check(nx,ny,m,n)){
+                    if(d+1<grid[nx][ny]){
+                        int val=grid[nx][ny];
+                        if((val+d+1)&1)
+                            val++;
+                        if(val<dis[nx][ny]){
+                            dis[nx][ny]=val;;
+                            ms.insert({val,nx,ny});
                         }
                     }
-                    else
-                    {
-                        //back and forth
-                        int tmp=grid[ni][nj];
-                        if((tmp+dist+1)&1)
-                            tmp++;
-                        if(tmp<dis[ni][nj])
-                        {
-                            dis[ni][nj]=tmp;
-                            q.push({tmp,ni,nj});
+                    else{
+                        if(d+1<dis[nx][ny]){
+                            dis[nx][ny]=d+1;
+                            ms.insert({dis[nx][ny],nx,ny});
                         }
                     }
                 }
             }
+            
         }
-        // for(int i=0;i<m;i++)
-        // {
-        //     for(int j=0;j<n;j++)
-        //         cout<<dis[i][j]<<" ";
-        //     cout<<endl;
-        // }
-        if(dis[m-1][n-1] == 1e9)
+        if(dis[m-1][n-1]==1e9)
             return -1;
         return dis[m-1][n-1];
-        
     }
 };
